47ba979406529ff438c982e31e9323a33955ddc6
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-09-06 02:58:06 +0000

    20240906 MM003 購買発注一括変更

---
+++ mm-s4apps01/pochange/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["mm/pochange/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ mm-s4apps01/pochange/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"mm/pochange/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
