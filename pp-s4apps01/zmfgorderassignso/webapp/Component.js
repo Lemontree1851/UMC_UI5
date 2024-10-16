62444be6ac5c33beb49858a5d68e4d64e889f02e
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-10-16 06:45:29 +0000

    PP-025 製造指図と受注の割当管理

---
+++ pp-s4apps01/zmfgorderassignso/webapp/Component.js
@@ -0,0 +1,12 @@
+sap.ui.define(
+    ["sap/suite/ui/generic/template/lib/AppComponent"],
+    function (Component) {
+        "use strict";
+
+        return Component.extend("pp.zmfgorderassignso.Component", {
+            metadata: {
+                manifest: "json"
+            }
+        });
+    }
+);
