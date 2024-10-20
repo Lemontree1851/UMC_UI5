dbc5f3a242d5d3df08fe64ff28d66b31996b0555
tianwen bao <tianwen.bao@sh.shin-china.com>
2024-10-20 03:14:44 +0000

    SD-031与信管理表

---
+++ sd-s4apps01/creditmantable/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("sd.creditmantable.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
---
+++ sd-s4apps01/creditmantable/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sd/creditmantable/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
