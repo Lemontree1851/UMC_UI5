32f50a3d57a2e96ebd7f99425c92182c12c8bff3
siyun <siyun.yao@sh.shin-china.com>
2024-10-22 08:49:34 +0000

    FICO-018 継続年齢アップデート

---
+++ fico-s4apps01/zinageupload/webapp/controller/App.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("fico.zinageupload.controller.App", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
