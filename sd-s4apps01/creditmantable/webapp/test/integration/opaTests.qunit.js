9b6cac810e4b060e9e4f5ca6594b8420adc516a0
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-10-23 01:20:39 +0000

    SD-031 与信管理表 Delete Duplicate

--- sd-s4apps01/creditmantable/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["sd/creditmantable/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- sd-s4apps01/creditmantable/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sd/creditmantable/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
