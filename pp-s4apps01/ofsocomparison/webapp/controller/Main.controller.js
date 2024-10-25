d7f27bf8b218bf06b02af1d12d41f3f00e566e17
tianwen bao <tianwen.bao@sh.shin-china.com>
2024-10-25 12:14:38 +0000

    PP-011 OFとSO比較照会

---
+++ pp-s4apps01/ofsocomparison/webapp/controller/Main.controller.js
@@ -0,0 +1,12 @@
+sap.ui.define([
+    "sap/ui/core/mvc/Controller"
+],
+function (Controller) {
+    "use strict";
+
+    return Controller.extend("pp.ofsocomparison.controller.Main", {
+        onInit: function () {
+
+        }
+    });
+});
---
+++ pp-s4apps01/ofsocomparison/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"pp/ofsocomparison/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
