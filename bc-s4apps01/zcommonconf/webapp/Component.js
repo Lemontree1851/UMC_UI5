759d756b9c139117fe324961a01f70c5d5123bc1
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-10-30 02:02:35 +0000

    BC - Fiori Apps

--- bc-s4apps01/zcommonconf/webapp/Component.js
+++ bc-s4apps01/zcommonconf/webapp/Component.js
@@ -1,12 +1,25 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "sap/m/MessageBox"
+], function (Component, MessageBox) {
+    "use strict";
 
-        return Component.extend("bc.zcommonconf.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("bc.zcommonconf.Component", {
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            var sMessage = "为方便数据管理，请大家维护配置时遵循以下几点:"
+                + "\n\n"
+                + "1、命名规范：Z + 模块 + 三位流水。"
+                + "\n\n"
+                + "2、配置完成后，请仔细检查数据的正确性。"
+                + "\n\n"
+                + "谢谢大家配合。"
+                + "\n\n"
+                + "许鑫磊";
+            MessageBox.information(sMessage);
+        }
+    });
+});
--- bc-s4apps01/zdtimpconf/webapp/Component.js
+++ bc-s4apps01/zdtimpconf/webapp/Component.js
@@ -1,12 +1,27 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "sap/m/MessageBox"
+], function (Component, MessageBox) {
+    "use strict";
 
-        return Component.extend("bc.zdtimpconf.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("bc.zdtimpconf.Component", {
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            var sMessage = "为方便数据管理，请大家遵循以下规范:"
+                + "\n\n"
+                + "1、批导对象，以 ZDATAIMPORT_ 开头。"
+                + "\n\n"
+                + "2、上传模版，以 ZUPLOAD_ 开头，Function 信息填 - 。"
+                + "\n\n"
+                + "3、下载模版，以 ZDOWNLOAD_ 开头，Function 信息填 - 。"
+                + "\n\n"
+                + "谢谢大家配合。"
+                + "\n\n"
+                + "许鑫磊";
+            MessageBox.information(sMessage);
+        }
+    });
+});
