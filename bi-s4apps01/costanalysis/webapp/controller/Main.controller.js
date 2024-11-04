fa7fdd25b568eb1995e8ac442216554afa368e3c
tianwen bao <tianwen.bao@sh.shin-china.com>
2024-11-04 08:19:08 +0000

    BI004 逆ザヤ防止分析

--- bi-s4apps01/costanalysis/webapp/controller/Main.controller.js
+++ bi-s4apps01/costanalysis/webapp/controller/Main.controller.js
@@ -16,29 +16,63 @@ function (Base,formatter, BusyDialog, MessageBox, MessageToast, Filter, FilterOp
  
         formatter: formatter,
         onInit: function () {
-
+       
         },
         onSearch: function () {
             this.getModel().resetChanges();
-			var oSearchBar = this.byId("idSmartFilterBar1");
-			var oTable = this.byId("tablelist");
-			var aFilters = oSearchBar.getFilters();
+			// // var oSearchBar = this.byId("idSmartFilterBar1");
+			// // var oTable = this.byId("tablelist");
+			// // var aFilters = oSearchBar.getFilters();
 
-			var sYear = this.getModel("local").getProperty("/zYear");
-			if (sYear) {
-				aFilters.push(new Filter("zYear", FilterOperator.EQ, sYear.getFullYear()));
-			}
-			var sMonth = this.getModel("local").getProperty("/zMonth");
-			if (sMonth) {
-				aFilters.push(new Filter("zMonth", FilterOperator.EQ, sMonth.getFullYear()));
-			}
+			// // var sYear = this.getModel("local").getProperty("/zYear");
+			// // if (sYear) {
+            // //     aFilters.push(new Filter("zYear", FilterOperator.EQ, sYear.getFullYear()));
+			// // }
+			// // var sMonth = this.getModel("local").getProperty("/zMonth");
+			// // if (sMonth) {
+			// // 	aFilters.push(new Filter("zMonth", FilterOperator.EQ, sMonth.getFullYear()));
+			// // }
 
-            oSearchBar.getFilters().push(new Filter("zYear", FilterOperator.EQ, sYear.getFullYear()));
+            // // // oSearchBar.getFilters().push(new Filter("zYear", FilterOperator.EQ, sYear.getFullYear()));
+            // // this.byId("idSmartFilterBar1").setFilterData(aFilters,true);   
+        
+            // // var bFilters = oSearchBar.getFilters();
+			// // var oFilterData = oSearchBar.getFilterData(); 
+            
+            // var filterData = this.byId("idSmartFilterBar1").getFilterData();
+            // var sYear = this.getModel("local").getProperty("/zYear").getFullYear();
+            // var oGjahr = "=" + sYear;
+            // var condition = {exclude: false, operation: "EQ", keyField: "zYear", tokenText:oGjahr, value1:sYear, value2: null};
+            // // if (filterData.zYear) {
+            // //  filterData.zYear.ranges[0] = condition;
+            // // } else {
+            // //  filterData.zYear = {items: [], ranges:[condition], value: null};
+            // // }
+            // filterData.zYear = sYear;
+            // this.byId("idSmartFilterBar1").setFilterData(filterData,true);    
+            // // this.byId("idSmartFilterBar1").setFiltes  
 
-            var bFilters = oSearchBar.getFilters();
-			var oFilterData = oSearchBar.getFilterData(); 
+            // var afilterData = this.byId("idSmartFilterBar1").getFilterData();
+
+ 
         },
+		onBeforeRebindTable: function (oEvent) {
 
+            var sYear = this.getModel("local").getProperty("/zYear").getFullYear();
+            var oYear = {oValue1:sYear, oValue2: null, sOperator: "EQ", sPath: "zYear", _bMultiFilter:false};
+
+            oEvent.getParameter("bindingParams").filters.push(oYear);
+
+            var sMonth = this.getModel("local").getProperty("/zMonth").getMonth() + 1;
+            var oMonth = {oValue1:sMonth, oValue2: null, sOperator: "EQ", sPath: "zMonth", _bMultiFilter:false};
+
+            oEvent.getParameter("bindingParams").filters.push(oMonth);
+
+
+
+
+
+		},
 
 
 
