c513f5a86f9c16830070df987a2254eccc67d15a
siyun <siyun.yao@sh.shin-china.com>
2024-11-06 11:42:34 +0000

    ysy del zinageupload

--- fico-s4apps01/zinageupload/webapp/test/integration/opaTests.qunit.js
+++
@@ -1,7 +0,0 @@
-/* global QUnit */
-
-sap.ui.require(["fico/zinageupload/test/integration/AllJourneys"
-], function () {
-	QUnit.config.autostart = false;
-	QUnit.start();
-});
--- fico-s4apps01/zinageupload/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"fico/zinageupload/test/unit/controller/View1.controller"
-], function () {
-	"use strict";
-});
