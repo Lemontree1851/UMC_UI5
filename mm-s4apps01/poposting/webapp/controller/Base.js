be5a7fdc50c51330c4473e1d01048efb2f96ac8d
liangxiaorui <liangxiaorui@outlook.com>
2024-11-07 06:28:06 +0000

    MM-020 Change Base.js Controller Name

--- mm-s4apps01/poposting/webapp/controller/Base.js
+++ mm-s4apps01/poposting/webapp/controller/Base.js
@@ -9,7 +9,7 @@ sap.ui.define([
 ], function (Controller, UIComponent, History, MessageBox, Fragment, Filter, FilterOperator) {
     "use strict";
 
-    return Controller.extend("pp.zbomupload.controller.Base", {
+    return Controller.extend("poposting.controller.Base", {
 
         /**
          * Convenience method for accessing the router in every controller of the application.
