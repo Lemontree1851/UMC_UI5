741a361cb69681dcb42e82c81e42e2c2903c6548
tianwen bao <tianwen.bao@sh.shin-china.com>
2024-11-15 06:08:58 +0000

    PP-011 OFとSO比較照会 busydialog

--- pp-s4apps01/ofsocomparison/webapp/controller/Main.controller.js
+++ pp-s4apps01/ofsocomparison/webapp/controller/Main.controller.js
@@ -2,13 +2,15 @@ sap.ui.define([
 	"./Base",
 	"sap/ui/table/Column",
 	"sap/m/Text",
+	"sap/m/MessageBox",
+	"sap/m/BusyDialog",
 	"sap/m/MessageToast",
 	"sap/ui/model/Filter",
 	"sap/ui/model/FilterOperator",
 	"pp/ofsocomparison/model/formatter",
 	'sap/ui/export/Spreadsheet',
 ],
-function (Base, Column, Text, MessageToast, Filter, FilterOperator, formatter,Spreadsheet) {
+function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterOperator, formatter,Spreadsheet) {
     "use strict";
 
     return Base.extend("pp.ofsocomparison.controller.Main", {
@@ -16,6 +18,7 @@ function (Base, Column, Text, MessageToast, Filter, FilterOperator, formatter,Sp
         formatter: formatter,
 
         onInit: function () {
+			this._BusyDialog = new BusyDialog();
 
         },
 
@@ -71,13 +74,25 @@ function (Base, Column, Text, MessageToast, Filter, FilterOperator, formatter,Sp
 
 
 			// Promise.all([this.readData(aFilters)]).then((results) => {
-
+			this._BusyDialog.open();
 			Promise.all([this.readData(aFilters)]).then((results) => {	
-				that.getModel("local").setProperty("/data", results[0].results);
-				that.buildListResultUITable(oTable, results[0].results[0]);
-			}).catch(() => {
+				if (results[0].results.length > 0) {
+					that.getModel("local").setProperty("/data", results[0].results);
+					that.buildListResultUITable(oTable, results[0].results[0]);
+					this._BusyDialog.close();
+                } else {
+					this._BusyDialog.close();
+                    MessageBox.error("No Data");
+					that.getModel("local").setProperty("/data", results[0].results);
+					that.buildListResultUITable(oTable, results[0].results[0]);	
+
+
+					
+                }	
+			}).catch((error) => {
+				MessageBox.error(error);
 			}).finally(() => {
-				that.setBusy(false);
+				this._BusyDialog.close();
 			});
 		},
 
@@ -104,416 +119,413 @@ function (Base, Column, Text, MessageToast, Filter, FilterOperator, formatter,Sp
 		},
 
 		buildListResultUITable: function (oTable, titleVariable) {
-			oTable.removeAllColumns();
+			if (titleVariable) {
+				oTable.removeAllColumns();
 
-			oTable.addColumn(new Column({
-				label: "{i18n>Plant}",
-				template: new Text({
-					text: "{local>Plant}"
-				}),
-				width: "10rem"
-			}));
-			oTable.addColumn(new Column({
-				label: "{i18n>Customer}",
-				template: new Text({
-					text: "{local>Customer}"
-				}),
-				width: "10rem"
-			}));
-			oTable.addColumn(new Column({
-				label: "{i18n>Material}",
-				template: new Text({
-					text: "{local>Material}"
-				}),
-				width: "10rem"
-			}));
-			oTable.addColumn(new Column({
-				label: "{i18n>MaterialName}",
-				template: new Text({
-					text: "{local>MaterialName}"
-				}),
-				width: "10rem"
-			}));
-			oTable.addColumn(new Column({
-				label: "{i18n>MATERIALBYCUSTOMER}",
-				template: new Text({
-					text: "{local>MATERIALBYCUSTOMER}"
-				}),
-				width: "10rem"
-			}));
-			oTable.addColumn(new Column({
-				label: "{i18n>CREATED_AT}",
-				template: new Text({
-					text: "{local>CREATED_AT}"
-				}),
-				width: "10rem"
-			}));
-
-			if (titleVariable.PeriodT1){
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT1,
+					label: "{i18n>Plant}",
 					template: new Text({
-						text: "{local>Period1}"
+						text: "{local>Plant}"
 					}),
 					width: "10rem"
 				}));
-			}
-
-			if (titleVariable.PeriodT2){
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT2,
+					label: "{i18n>Customer}",
 					template: new Text({
-						text: "{local>Period2}"
-					}),
-					width: "10rem"
-				}));	
-			}
-
-			if (titleVariable.PeriodT3){	
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT3,
-					template: new Text({
-						text: "{local>Period3}"
-					}),
-					width: "10rem"
-				}));		
-			}
-
-			if (titleVariable.PeriodT4){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT4,
-					template: new Text({
-						text: "{local>Period4}"
-					}),
-					width: "10rem"
-				}));	
-			}
-
-			if (titleVariable.PeriodT5){
-			
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT5,
-					template: new Text({
-						text: "{local>Period5}"
+						text: "{local>Customer}"
 					}),
 					width: "10rem"
 				}));
-			}
-
-			if (titleVariable.PeriodT6){
-
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT6,
+					label: "{i18n>Material}",
 					template: new Text({
-						text: "{local>Period6}"
+						text: "{local>Material}"
 					}),
 					width: "10rem"
 				}));
-			}
-
-			if (titleVariable.PeriodT7){
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT7,
+					label: "{i18n>MaterialName}",
 					template: new Text({
-						text: "{local>Period7}"
+						text: "{local>MaterialName}"
 					}),
 					width: "10rem"
 				}));
-			}
-
-			if (titleVariable.PeriodT8){
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT8,
+					label: "{i18n>MATERIALBYCUSTOMER}",
 					template: new Text({
-						text: "{local>Period8}"
+						text: "{local>MATERIALBYCUSTOMER}"
 					}),
 					width: "10rem"
 				}));
-			}
-
-			if (titleVariable.PeriodT9){
 				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT9,
+					label: "{i18n>CREATED_AT}",
 					template: new Text({
-						text: "{local>Period9}"
+						text: "{local>CREATED_AT}"
 					}),
 					width: "10rem"
 				}));
-			}
 
-			if (titleVariable.PeriodT10){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT10,
-					template: new Text({
-						text: "{local>Period10}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT1){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT1,
+						template: new Text({
+							text: "{local>Period1}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT11){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT11,
-					template: new Text({
-						text: "{local>Period11}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT2){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT2,
+						template: new Text({
+							text: "{local>Period2}"
+						}),
+						width: "10rem"
+					}));	
+				}
 
-			if (titleVariable.PeriodT12){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT12,
-					template: new Text({
-						text: "{local>Period12}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT3){	
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT3,
+						template: new Text({
+							text: "{local>Period3}"
+						}),
+						width: "10rem"
+					}));		
+				}
 
-			if (titleVariable.PeriodT13){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT13,
-					template: new Text({
-						text: "{local>Period13}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT4){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT4,
+						template: new Text({
+							text: "{local>Period4}"
+						}),
+						width: "10rem"
+					}));	
+				}
 
-			if (titleVariable.PeriodT14){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT14,
-					template: new Text({
-						text: "{local>Period14}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT5){
+				
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT5,
+						template: new Text({
+							text: "{local>Period5}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT15){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT15,
-					template: new Text({
-						text: "{local>Period15}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT6){
 
-			if (titleVariable.PeriodT16){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT16,
-					template: new Text({
-						text: "{local>Period16}"
-					}),
-					width: "10rem"
-				}));
-			}
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT6,
+						template: new Text({
+							text: "{local>Period6}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT17){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT17,
-					template: new Text({
-						text: "{local>Period17}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT7){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT7,
+						template: new Text({
+							text: "{local>Period7}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT18){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT18,
-					template: new Text({
-						text: "{local>Period18}"
-					}),
-					width: "10rem"
-				}));
-			}
-			if (titleVariable.PeriodT19){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT19,
-					template: new Text({
-						text: "{local>Period19}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT8){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT8,
+						template: new Text({
+							text: "{local>Period8}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT20){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT20,
-					template: new Text({
-						text: "{local>Period20}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT9){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT9,
+						template: new Text({
+							text: "{local>Period9}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT21){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT21,
-					template: new Text({
-						text: "{local>Period21}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT10){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT10,
+						template: new Text({
+							text: "{local>Period10}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT22){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT22,
-					template: new Text({
-						text: "{local>Period22}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT11){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT11,
+						template: new Text({
+							text: "{local>Period11}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT23){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT23,
-					template: new Text({
-						text: "{local>Period23}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT12){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT12,
+						template: new Text({
+							text: "{local>Period12}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT24){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT24,
-					template: new Text({
-						text: "{local>Period24}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT13){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT13,
+						template: new Text({
+							text: "{local>Period13}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT25){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT25,
-					template: new Text({
-						text: "{local>Period25}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT14){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT14,
+						template: new Text({
+							text: "{local>Period14}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT26){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT26,
-					template: new Text({
-						text: "{local>Period26}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT15){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT15,
+						template: new Text({
+							text: "{local>Period15}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT27){v
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT27,
-					template: new Text({
-						text: "{local>Period27}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT16){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT16,
+						template: new Text({
+							text: "{local>Period16}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT28){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT28,
-					template: new Text({
-						text: "{local>Period28}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT17){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT17,
+						template: new Text({
+							text: "{local>Period17}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT29){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT29,
-					template: new Text({
-						text: "{local>Period29}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT18){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT18,
+						template: new Text({
+							text: "{local>Period18}"
+						}),
+						width: "10rem"
+					}));
+				}
+				if (titleVariable.PeriodT19){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT19,
+						template: new Text({
+							text: "{local>Period19}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT30){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT30,
-					template: new Text({
-						text: "{local>Period30}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT20){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT20,
+						template: new Text({
+							text: "{local>Period20}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT31){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT31,
-					template: new Text({
-						text: "{local>Period31}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT21){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT21,
+						template: new Text({
+							text: "{local>Period21}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT32){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT32,
-					template: new Text({
-						text: "{local>Period32}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT22){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT22,
+						template: new Text({
+							text: "{local>Period22}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT33){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT33,
-					template: new Text({
-						text: "{local>Period33}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT23){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT23,
+						template: new Text({
+							text: "{local>Period23}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT34){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT34,
-					template: new Text({
-						text: "{local>Period34}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT24){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT24,
+						template: new Text({
+							text: "{local>Period24}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT35){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT35,
-					template: new Text({
-						text: "{local>Period35}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT25){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT25,
+						template: new Text({
+							text: "{local>Period25}"
+						}),
+						width: "10rem"
+					}));
+				}
 
-			if (titleVariable.PeriodT36){
-				oTable.addColumn(new Column({
-					label: titleVariable.PeriodT36,
-					template: new Text({
-						text: "{local>Period36}"
-					}),
-					width: "10rem"
-				}));
-			}
+				if (titleVariable.PeriodT26){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT26,
+						template: new Text({
+							text: "{local>Period26}"
+						}),
+						width: "10rem"
+					}));
+				}
 
+				if (titleVariable.PeriodT27){v
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT27,
+						template: new Text({
+							text: "{local>Period27}"
+						}),
+						width: "10rem"
+					}));
+				}
 
+				if (titleVariable.PeriodT28){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT28,
+						template: new Text({
+							text: "{local>Period28}"
+						}),
+						width: "10rem"
+					}));
+				}
 
+				if (titleVariable.PeriodT29){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT29,
+						template: new Text({
+							text: "{local>Period29}"
+						}),
+						width: "10rem"
+					}));
+				}
 
+				if (titleVariable.PeriodT30){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT30,
+						template: new Text({
+							text: "{local>Period30}"
+						}),
+						width: "10rem"
+					}));
+				}
 
+				if (titleVariable.PeriodT31){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT31,
+						template: new Text({
+							text: "{local>Period31}"
+						}),
+						width: "10rem"
+					}));
+				}
+
+				if (titleVariable.PeriodT32){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT32,
+						template: new Text({
+							text: "{local>Period32}"
+						}),
+						width: "10rem"
+					}));
+				}
+
+				if (titleVariable.PeriodT33){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT33,
+						template: new Text({
+							text: "{local>Period33}"
+						}),
+						width: "10rem"
+					}));
+				}
+
+				if (titleVariable.PeriodT34){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT34,
+						template: new Text({
+							text: "{local>Period34}"
+						}),
+						width: "10rem"
+					}));
+				}
+
+				if (titleVariable.PeriodT35){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT35,
+						template: new Text({
+							text: "{local>Period35}"
+						}),
+						width: "10rem"
+					}));
+				}
+
+				if (titleVariable.PeriodT36){
+					oTable.addColumn(new Column({
+						label: titleVariable.PeriodT36,
+						template: new Text({
+							text: "{local>Period36}"
+						}),
+						width: "10rem"
+					}));
+				}
+		    }
 
 		},
 
