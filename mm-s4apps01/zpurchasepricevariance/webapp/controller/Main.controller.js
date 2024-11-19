da430b5725d5eea3cc5b1bbfc7d715d55185ba8a
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-11-19 03:37:53 +0000

    MM-004 購買単価差異レポート 画面优化调整

--- mm-s4apps01/zpurchasepricevariance/webapp/controller/Main.controller.js
+++ mm-s4apps01/zpurchasepricevariance/webapp/controller/Main.controller.js
@@ -27,6 +27,52 @@ sap.ui.define([
                 });
                 aFilters.push(oNewFilter);
             }
+        },
+
+        onBeforeExport: function (oEvent) {
+            var mExcelSettings = oEvent.getParameter("exportSettings");
+            var sFileName = this.getModel("i18n").getResourceBundle().getText("appTitle");
+            this._exportExcel(mExcelSettings, sFileName);
+        },
+
+        _exportExcel: function (mExcelSettings, sFileName) {
+            mExcelSettings.workbook.columns.forEach(function (oColumn) {
+                switch (oColumn.property) {
+                    //  Date
+                    case "PurchaseOrderDate":
+                    case "ScheduleLineDeliveryDate":
+                    case "DeliveryDate":
+                    case "PostingDate":
+                    case "ConditionValidityStartDate":
+                    case "ConditionValidityEndDate":
+                    case "PurgDocPriceDate":
+                    case "PriceDate":
+                        oColumn.type = sap.ui.export.EdmType.Date;
+                        break;
+                    //  Number 分隔符 没有小数位
+                    case "CurrentPrice":
+                    case "NewPrice":
+                    case "Difference":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.scale = 3;
+                        oColumn.textAlign = "End";
+                        break;
+                    case "OrderQuantity":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.scale = 2;
+                        oColumn.textAlign = "End";
+                        break;
+                    case "NetPriceQuantity":
+                    case "ConditionQuantity":
+                        oColumn.type = sap.ui.export.EdmType.Number;
+                        oColumn.delimiter = true;
+                        oColumn.textAlign = "End";
+                        break;
+                }
+            });
+            mExcelSettings.fileName = sFileName + "_" + this.getCurrentDateTime();
         }
     });
 });
