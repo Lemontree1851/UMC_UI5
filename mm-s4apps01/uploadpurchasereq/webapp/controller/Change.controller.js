3f9be8d45cc09a5d16b7f97899e82364d56bb65c
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-11-21 06:30:20 +0000

    20241121 Update Attachment path

--- mm-s4apps01/uploadpurchasereq/webapp/controller/Change.controller.js
+++ mm-s4apps01/uploadpurchasereq/webapp/controller/Change.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define([
-    "./BaseController",
-    "../model/formatter",
-    "./messages",
+	"./BaseController",
+	"../model/formatter",
+	"./messages",
 	"sap/m/MessageBox",
 	"sap/ui/core/Messaging",
 	"sap/m/MessageToast"
-], function(
-    BaseController,
-    formatter,
-    messages,
+], function (
+	BaseController,
+	formatter,
+	messages,
 	MessageBox,
 	Messaging,
 	MessageToast
@@ -16,13 +16,13 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("mm.uploadpurchasereq.controller.Change", {
-        formatter : formatter,
-        onInit: function () {
-            // this._BusyDialog = new BusyDialog();
+		formatter: formatter,
+		onInit: function () {
+			// this._BusyDialog = new BusyDialog();
 			this._LocalData = this.getOwnerComponent().getModel("local");
-            this._oDataModel = this.getOwnerComponent().getModel();
-            this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            var oRouter = this.getRouter();
+			this._oDataModel = this.getOwnerComponent().getModel();
+			this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
+			var oRouter = this.getRouter();
 			oRouter.getRoute("PurchaseReq").attachMatched(this._onRouteMatched, this);
 			// oRouter.attachBeforeRouteMatched(this._onBeforeRouteMatched, this);
 
@@ -34,7 +34,7 @@ sap.ui.define([
 
 			this._timeline = this.byId("idTimeline");
 			this._timeline.setEnableScroll(false);
-        },
+		},
 		getMediaUrl: function (sUrlString) {
 			if (sUrlString) {
 				var sUrl = new URL(sUrlString);
@@ -46,16 +46,16 @@ sap.ui.define([
 				return "";
 			}
 		},
-        _onRouteMatched : function (oEvent) {
+		_onRouteMatched: function (oEvent) {
 			var oArgs, oView;
 
 			oArgs = oEvent.getParameter("arguments");
 			oView = this.getView();
- 			this._InsNo3 = oArgs.contextInstanceId;
- 			this._InsNo4 = oArgs.contextApplicationId;
+			this._InsNo3 = oArgs.contextInstanceId;
+			this._InsNo4 = oArgs.contextApplicationId;
 			oView.bindElement({
-				path : "/PurchaseReq(guid'" + oArgs.contextPath + "')",
-				events : {
+				path: "/PurchaseReq(guid'" + oArgs.contextPath + "')",
+				events: {
 					change: this._onBindingChange.bind(this),
 					dataRequested: function (oEvent) {
 						oView.setBusy(true);
@@ -69,7 +69,7 @@ sap.ui.define([
 			this.byId("idPage").setShowFooter(false);
 			this._bindTimelineAggregation();
 		},
-		
+
 		// 应该在未保存返回时提醒，但目前如果使用launchPad自带的返回无法终止导航，所以暂时没有使用
 		// _onBeforeRouteMatched: function (oEvent) {
 		// 	// 检查是否允许导航
@@ -89,7 +89,7 @@ sap.ui.define([
 		// 	var a = 12;
 		// 	return !this.getView().getModel().hasPendingChanges();
 		// },
-		_onBindingChange : function (oEvent) {
+		_onBindingChange: function (oEvent) {
 			// No data for the binding
 			if (!this.getView().getBindingContext()) {
 				this.getRouter().getTargets().display("notFound");
@@ -102,7 +102,7 @@ sap.ui.define([
 				// 	this._bindUploadSetUrl(oUploadSet,item);
 				// }
 				this.getInfoRecord(item).then(function (res) {
-					this._bindUploadSetUrl(oUploadSet,res);
+					this._bindUploadSetUrl(oUploadSet, res);
 				}.bind(this)).catch(function () {
 					oUploadSet.unbindElement("Attach");
 					oUploadSet.setUploadUrl();
@@ -134,31 +134,31 @@ sap.ui.define([
 		onBeforeItemAdded: function (oEvent) {
 			var oUploadSet = oEvent.getSource();
 			var sFileName = encodeURIComponent(oEvent.getParameter("item").getFileName());
-			if(oUploadSet.getUploadUrl()) {
+			if (oUploadSet.getUploadUrl()) {
 				this._saveAttachment(oUploadSet, sFileName);
 			} else {
 				// 创建采购申请的文档信息记录
 				var item = this.getView().getBindingContext().getObject();
-				var sInfoRecordDocNumber = item.PrNo + item.PrItem.padStart(5,"0") + item.UUID.slice(-10).toUpperCase();
-				this.createInfoRecord(sInfoRecordDocNumber).then( function (res) {
-					this._bindUploadSetUrl(oUploadSet,res);
+				var sInfoRecordDocNumber = item.PrNo + item.PrItem.padStart(5, "0") + item.UUID.slice(-10).toUpperCase();
+				this.createInfoRecord(sInfoRecordDocNumber).then(function (res) {
+					this._bindUploadSetUrl(oUploadSet, res);
 					this._saveAttachment(oUploadSet, sFileName);
 					//同时将InfoRecordDocNumber写入采购申请中
 					// var sPath = this.getView().getBindingContext().sPath;
 					// this._oDataModel.setProperty(sPath + "/DocumentInfoRecordDocNumber", sInfoRecordDocNumber);
 				}.bind(this));
 			}
-            
-        },
+
+		},
 		onUploadCompleted: function (oEvent) {
 			oEvent.getSource().getBinding("items").refresh();
 		},
 		getInfoRecord: function (item) {
-			var sInfoRecordDocNumber = item.PrNo + item.PrItem.padStart(5,"0") + item.UUID.slice(-10).toUpperCase();
+			var sInfoRecordDocNumber = item.PrNo + item.PrItem.padStart(5, "0") + item.UUID.slice(-10).toUpperCase();
 			var oInfoRecordModel = this.getView().getModel("InfoRecord");
 			const sPath = "/A_DocumentInfoRecord(DocumentInfoRecordDocType='SAT',DocumentInfoRecordDocNumber='" +
 				sInfoRecordDocNumber + "',DocumentInfoRecordDocVersion='00',DocumentInfoRecordDocPart='000')";
-			var promise = new Promise (function (resolve, reject) {
+			var promise = new Promise(function (resolve, reject) {
 				var mParameters = {
 					success: function (oData) {
 						resolve(oData);
@@ -180,7 +180,7 @@ sap.ui.define([
 				"DocumentInfoRecordDocVersion": "00",
 				"DocumentInfoRecordDocPart": "000"
 			};
-			var promise = new Promise (function (resolve, reject) {
+			var promise = new Promise(function (resolve, reject) {
 				var mParameters = {
 					success: function (oData) {
 						resolve(oData);
@@ -196,7 +196,7 @@ sap.ui.define([
 		_bindUploadSetUrl: function (oUploadSet, item) {
 			const sDocumentInfoRecordDocNumber = item.DocumentInfoRecordDocNumber;
 			const sPath = "Attach>/A_DocumentInfoRecordAttch(DocumentInfoRecordDocType='SAT',DocumentInfoRecordDocNumber='" +
-			sDocumentInfoRecordDocNumber + "',DocumentInfoRecordDocVersion='00',DocumentInfoRecordDocPart='000')";
+				sDocumentInfoRecordDocNumber + "',DocumentInfoRecordDocVersion='00',DocumentInfoRecordDocPart='000')";
 			oUploadSet.bindElement(sPath);
 			// 设置uploadUrl
 			// const sUploadUrl = `/sap/opu/odata/sap/API_CV_ATTACHMENT_SRV/A_DocumentInfoRecordAttch(
@@ -204,7 +204,9 @@ sap.ui.define([
 			// 	DocumentInfoRecordDocNumber='${item.DocumentInfoRecordDocNumber}',
 			// 	DocumentInfoRecordDocVersion='${item.DocumentInfoRecordDocVersion}',
 			// 	DocumentInfoRecordDocPart='${item.DocumentInfoRecordDocPart}')`;
-			const sUploadUrl = "/sap/opu/odata/sap/API_CV_ATTACHMENT_SRV/A_DocumentInfoRecordAttch(DocumentInfoRecordDocType='SAT',DocumentInfoRecordDocNumber='" +
+			// const sUploadUrl = "/sap/opu/odata/sap/API_CV_ATTACHMENT_SRV/A_DocumentInfoRecordAttch(DocumentInfoRecordDocType='SAT',DocumentInfoRecordDocNumber='" +
+			// 	sDocumentInfoRecordDocNumber + "',DocumentInfoRecordDocVersion='00',DocumentInfoRecordDocPart='000')/DocumentInfoRecordToAttachmentNavigation";
+			const sUploadUrl = this.getView().getModel("Attach").sServiceUrl + "/A_DocumentInfoRecordAttch(DocumentInfoRecordDocType='SAT',DocumentInfoRecordDocNumber='" +
 				sDocumentInfoRecordDocNumber + "',DocumentInfoRecordDocVersion='00',DocumentInfoRecordDocPart='000')/DocumentInfoRecordToAttachmentNavigation";
 			oUploadSet.setUploadUrl(sUploadUrl);
 		},
@@ -214,10 +216,10 @@ sap.ui.define([
 			csrfToken.then(function (res) {
 				oUploadSet.removeAllHeaderFields();
 				oUploadSet.addHeaderField(
-					new sap.ui.core.Item({key:"x-csrf-token", text:res})
+					new sap.ui.core.Item({ key: "x-csrf-token", text: res })
 				);
 				oUploadSet.addHeaderField(
-					new sap.ui.core.Item({key:"slug", text:sFileName})
+					new sap.ui.core.Item({ key: "slug", text: sFileName })
 				);
 				oUploadSet.upload();
 			});
@@ -225,7 +227,7 @@ sap.ui.define([
 		onFileRenamed: function (oEvent) {
 			var oUploadSet = oEvent.getSource();
 			var sFileName = encodeURIComponent(oEvent.getParameter("item").getFileName());
-			this._saveAttachment(oUploadSet,sFileName);
+			this._saveAttachment(oUploadSet, sFileName);
 		},
 		onBeforeItemRemoved: function (oEvent) {
 			oEvent.preventDefault();
@@ -263,8 +265,8 @@ sap.ui.define([
 		_getMessagePopover() {
 			if (!this.MessageDialog) {
 				this.MessageDialog = this.loadFragment({
-										name: "mm.uploadpurchasereq.fragment.MessagePopover"
-									});
+					name: "mm.uploadpurchasereq.fragment.MessagePopover"
+				});
 			}
 
 			return this.MessageDialog;
@@ -281,21 +283,21 @@ sap.ui.define([
 			this._bindTimelineAggregation();
 		},
 		_bindTimelineAggregation: function () {
- 
+
 			var afilters = [];
 			var ApplicationId = "00" + this._InsNo4;
 			var oFilter1 = new sap.ui.model.Filter("WorkflowId", sap.ui.model.FilterOperator.EQ, "purchaserequisition");
 			var oFilter2 = new sap.ui.model.Filter("InstanceId", sap.ui.model.FilterOperator.EQ, this._InsNo3);
 			var oFilter3 = new sap.ui.model.Filter("ApplicationId", sap.ui.model.FilterOperator.EQ, ApplicationId);
 			afilters.push(oFilter1);
-			afilters.push( oFilter2);
-			
+			afilters.push(oFilter2);
+
 			afilters.push(oFilter3);
-			console.log("afilters",afilters);
+			console.log("afilters", afilters);
 			//afilters.push(oFilter1, oFilter2, oFilter3);
 			this._timeline.bindAggregation("content", {
 				path: "/ApprovalHistory",
-				filters:afilters,
+				filters: afilters,
 				template: this.byId("idTemplateItem").clone()
 			});
 		},
