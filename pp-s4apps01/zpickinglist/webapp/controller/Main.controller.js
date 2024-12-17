90db322dd73e55dc325c9870a994e6571f7f02f0
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-17 06:26:32 +0000

    Picking List 权限检查

--- pp-s4apps01/zpickinglist/webapp/controller/Main.controller.js
+++ pp-s4apps01/zpickinglist/webapp/controller/Main.controller.js
@@ -76,7 +76,6 @@ sap.ui.define([
         _initialize: function () {
             var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
             var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
-            sEmail = "xinlei.xu@sh.shin-china.com";
             var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
                 "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
             });
