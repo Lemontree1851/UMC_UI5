2bb65e8d7f345c1d1fa0f16e58a2d57ca7d2a0d2
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-20 10:40:03 +0000

    PP-033 製造指図情報の出力 权限检查

--- pp-s4apps01/zmfgorderinfo/webapp/Component.js
+++ pp-s4apps01/zmfgorderinfo/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "pp/zmfgorderinfo/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("pp.zmfgorderinfo.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("pp.zmfgorderinfo.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            ListReportExt.init(this.oModels);
+        }
+    });
+});
