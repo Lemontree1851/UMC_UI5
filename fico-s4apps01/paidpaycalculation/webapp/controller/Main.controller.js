4b59b3ada0c1e32855d881eccb16949cc51bb096
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-12-23 05:40:15 +0000

    20241223 FICO-007 有償支給純額計算処理

--- fico-s4apps01/paidpaycalculation/webapp/controller/Main.controller.js
+++ fico-s4apps01/paidpaycalculation/webapp/controller/Main.controller.js
@@ -16,8 +16,67 @@ sap.ui.define([
 				this._LocalData = this.getOwnerComponent().getModel("local");
 				this._oDataModel = this.getOwnerComponent().getModel();
 				this._BusyDialog = new BusyDialog();
+				//this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
+				
 			},
 
+			_initialize: function () {
+				this._UserInfo = sap.ushell.Container.getService("UserInfo");
+                var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                    "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+                });
+                oContextBinding.requestObject().then(function (context) {
+                    var aAccessBtns = [],
+                        aAllAccessBtns = [];
+                    if (context._AssignRole && context._AssignRole.length > 0) {
+                        context._AssignRole.forEach(role => {
+                            aAccessBtns.push(role._UserRoleAccessBtn);
+                        });
+                        aAllAccessBtns = aAccessBtns.flat();
+                    }
+                    if (!aAllAccessBtns.some(btn => btn.AccessId === "paidpaycalculation-View")) {
+                        if (!this.oErrorMessageDialog) {
+                            this.oErrorMessageDialog = new sap.m.Dialog({
+                                type: sap.m.DialogType.Message,
+                                state: "Error",
+                                content: new sap.m.Text({
+                                    text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                                })
+                            });
+                        }
+                        this.oErrorMessageDialog.open();
+                    }
+                    this.getModel("local").setProperty("/authorityCheck", {
+                        button: {
+                            View: aAllAccessBtns.some(btn => btn.AccessId === "paidpaycalculation-View"),
+							Material: aAllAccessBtns.some(btn => btn.AccessId === "paidpaycalculation-MaterialCalculate"),
+							PurchasingGroup: aAllAccessBtns.some(btn => btn.AccessId === "paidpaycalculation-PurchasingGroupCalculate"),
+                            Calc: aAllAccessBtns.some(btn => btn.AccessId === "paidpaycalculation-Calculation")
+                        },
+                        data: {
+                            PlantSet: context._AssignPlant,
+                            CompanySet: context._AssignCompany,
+                            SalesOrgSet: context._AssignSalesOrg,
+                            PurchOrgSet: context._AssignPurchOrg,
+                            RoleSet: context._AssignRole
+                        }
+                    });
+                }.bind(this), function (oError) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }.bind(this));
+			
+			},
 			onButtonSelect: function (oEvent) {
 				var sOption1 = this.byId("Option1").getSelected();
 				if (sOption1 === true) {
--- fico-s4apps01/paidpaydocument/webapp/controller/Main.controller.js
+++ fico-s4apps01/paidpaydocument/webapp/controller/Main.controller.js
@@ -16,6 +16,65 @@ sap.ui.define([
                 this._LocalData = this.getOwnerComponent().getModel("local");
                 this._oDataModel = this.getOwnerComponent().getModel();
                 this._BusyDialog = new BusyDialog();
+                this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
+            },
+
+            _initialize: function () {
+                this._UserInfo = sap.ushell.Container.getService("UserInfo");
+                var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                    "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+                });
+                oContextBinding.requestObject().then(function (context) {
+                    var aAccessBtns = [],
+                        aAllAccessBtns = [];
+                    if (context._AssignRole && context._AssignRole.length > 0) {
+                        context._AssignRole.forEach(role => {
+                            aAccessBtns.push(role._UserRoleAccessBtn);
+                        });
+                        aAllAccessBtns = aAccessBtns.flat();
+                    }
+                    if (!aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-View")) {
+                        if (!this.oErrorMessageDialog) {
+                            this.oErrorMessageDialog = new sap.m.Dialog({
+                                type: sap.m.DialogType.Message,
+                                state: "Error",
+                                content: new sap.m.Text({
+                                    text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                                })
+                            });
+                        }
+                        this.oErrorMessageDialog.open();
+                    }
+                    this.getModel("local").setProperty("/authorityCheck", {
+                        button: {
+                            View: aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-View"),
+                            RevenueCost: aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-RevenueCost"),
+                            APAR: aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-AccountReceivable AccountPayable"),
+                            Post: aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-Post"),
+                            Reserve: aAllAccessBtns.some(btn => btn.AccessId === "paidpaydocument-Reserve")
+                        },
+                        data: {
+                            PlantSet: context._AssignPlant,
+                            CompanySet: context._AssignCompany,
+                            SalesOrgSet: context._AssignSalesOrg,
+                            PurchOrgSet: context._AssignPurchOrg,
+                            RoleSet: context._AssignRole
+                        }
+                    });
+                }.bind(this), function (oError) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }.bind(this));
             },
 
             onButtonSelect: function (oEvent) {
@@ -154,8 +213,8 @@ sap.ui.define([
                             let result = JSON.parse(item["processLogic"].Zzkey);
                             that.getView().byId("SFBDocument").search();
                             // result.forEach(function (line) {
-                                
-                                
+
+
                             // });
                         });
 
@@ -211,7 +270,7 @@ sap.ui.define([
                 return postDocs;
             },
 
-            postAction: function (postData, sType, bEvent,sYear, sMonat) {
+            postAction: function (postData, sType, bEvent, sYear, sMonat) {
                 var oModel = this._oDataModel;
 
                 return new Promise(
