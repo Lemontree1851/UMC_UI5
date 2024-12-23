401853e259d89d02bd682e1e946604bca91205cb
YI RAO <yi.rao@sh.shin-china.com>
2024-12-23 08:04:37 +0000

    PP-002 BOMのレポート 20241223

--- pp-s4apps01/zbom/webapp/Component.js
+++ pp-s4apps01/zbom/webapp/Component.js
@@ -1,11 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "pp/zbom/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("pp.zbom.Component", {
+
+            ListReportExt: ListReportExt,
+
             metadata: {
                 manifest: "json"
+            },
+
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
