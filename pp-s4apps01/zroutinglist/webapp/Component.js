b04b9dc2cb32434ca723f0098193241bd4400bc5
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-23 03:34:24 +0000

    PP-005 作業手順一覧レポート 权限检查

--- pp-s4apps01/zroutinglist/webapp/Component.js
+++ pp-s4apps01/zroutinglist/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "pp/zroutinglist/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("pp.zroutinglist.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("pp.zroutinglist.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            ListReportExt.init(this.oModels);
+        }
+    });
+});
