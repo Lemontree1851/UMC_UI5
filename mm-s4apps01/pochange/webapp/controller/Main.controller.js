ea73e930867a8224afbc1a31bd99d3cbed91647f
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-12-24 05:23:03 +0000

    20241224 MM-003 購買発注一括変更

--- mm-s4apps01/pochange/webapp/controller/Main.controller.js
+++ mm-s4apps01/pochange/webapp/controller/Main.controller.js
@@ -18,7 +18,57 @@ sap.ui.define([
 
             _initialize: function () {
                 this._BusyDialog = new BusyDialog();
-
+                this._UserInfo = sap.ushell.Container.getService("UserInfo");
+                var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                    "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+                });
+                oContextBinding.requestObject().then(function (context) {
+                    var aAccessBtns = [],
+                        aAllAccessBtns = [];
+                    if (context._AssignRole && context._AssignRole.length > 0) {
+                        context._AssignRole.forEach(role => {
+                            aAccessBtns.push(role._UserRoleAccessBtn);
+                        });
+                        aAllAccessBtns = aAccessBtns.flat();
+                    }
+                    if (!aAllAccessBtns.some(btn => btn.AccessId === "pochange-View")) {
+                        if (!this.oErrorMessageDialog) {
+                            this.oErrorMessageDialog = new sap.m.Dialog({
+                                type: sap.m.DialogType.Message,
+                                state: "Error",
+                                content: new sap.m.Text({
+                                    text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                                })
+                            });
+                        }
+                        this.oErrorMessageDialog.open();
+                    }
+                    this.getModel("local").setProperty("/authorityCheck", {
+                        button: {
+                            View: aAllAccessBtns.some(btn => btn.AccessId === "pochange-View")
+                        },
+                        data: {
+                            PlantSet: context._AssignPlant,
+                            CompanySet: context._AssignCompany,
+                            SalesOrgSet: context._AssignSalesOrg,
+                            PurchOrgSet: context._AssignPurchOrg,
+                            RoleSet: context._AssignRole
+                        }
+                    });
+                }.bind(this), function (oError) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }.bind(this));
             },
 
             onFileChange: function (oEvent) {
@@ -276,7 +326,7 @@ sap.ui.define([
                             label: sLabelText,
                             // 数据类型，即设置excel该列的数据类型
                             type: (sTemplatePath === "ScheduleLineDeliveryDate" || sTemplatePath === "PurgDocPriceDate") ? "date" : "string",
-						    format: (sTemplatePath === "ScheduleLineDeliveryDate" || sTemplatePath === "PurgDocPriceDate") ? "yyyy/MM/dd" : "",
+                            format: (sTemplatePath === "ScheduleLineDeliveryDate" || sTemplatePath === "PurgDocPriceDate") ? "yyyy/MM/dd" : "",
 
                             // 获取数据的绑定路径，即设置excel该列的字段路径
                             property: sTemplatePath,
