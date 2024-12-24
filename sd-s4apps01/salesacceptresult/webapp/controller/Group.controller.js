8445f784ca8d574a01b731249eb85ce3f7881df8
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-12-24 08:43:43 +0000

    20241224 SD011 売上検収突合プラットフォーム

--- sd-s4apps01/salesacceptresult/webapp/controller/Group.controller.js
+++ sd-s4apps01/salesacceptresult/webapp/controller/Group.controller.js
@@ -15,6 +15,7 @@ sap.ui.define([
             this._LocalData = this.getOwnerComponent().getModel("local");
             this._oDataModel = this.getOwnerComponent().getModel();
             this._BusyDialog = new BusyDialog();
+            this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
 
             var oRouter = this.getOwnerComponent().getRouter();
             oRouter.getRoute("Group").attachPatternMatched(this._onRouteMatched, this);
@@ -41,6 +42,61 @@ sap.ui.define([
                     aFilters.push(oFilterItem);
                 }
             }
+        },
+
+        _initialize: function () {
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-View"),
+                        Finish: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-Finish")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         onBeforeRebindTable: function (oEvent, arg1, arg2, arg3, arg4) {
--- sd-s4apps01/salesacceptresult/webapp/controller/Main.controller.js
+++ sd-s4apps01/salesacceptresult/webapp/controller/Main.controller.js
@@ -15,6 +15,7 @@ sap.ui.define([
             this._oDataModel = this.getOwnerComponent().getModel();
             this._BusyDialog = new BusyDialog();
             this._oDataModel.setRefreshAfterChange(false);
+            this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
 
             this._oDataModel.attachBatchRequestCompleted(function (oEvent) {
                 this.setBusy(false);
@@ -45,6 +46,62 @@ sap.ui.define([
             // Example: Refresh a model or call a service
             this._oDataModel.setRefreshAfterChange(false);
             this._oDataModel.refresh();
+        },
+
+        _initialize: function () {
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-View"),
+                        Save: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-Save"),
+                        Navigate: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptresult-Navigate")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         onBeforeRebindTable: function (oEvent, arg1, arg2, arg3, arg4) {
