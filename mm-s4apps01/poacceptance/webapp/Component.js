ae15aa73a035edb5b375f9d1b5b11a357ce4066d
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-12-24 05:51:33 +0000

    20241224 MM-031 受入実績レポート

--- mm-s4apps01/poacceptance/webapp/Component.js
+++ mm-s4apps01/poacceptance/webapp/Component.js
@@ -1,11 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "mm/poacceptance/ext/controller/ListReportExt"
+],
+    function (Component, ListReportExt) {
         "use strict";
-
+   
         return Component.extend("mm.poacceptance.Component", {
+            ListReportExt: ListReportExt,
+
             metadata: {
                 manifest: "json"
+            },
+
+            onAfterRendering: function() {
+                ListReportExt.init(this.oModels);
             }
         });
     }
