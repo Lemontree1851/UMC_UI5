65615e240d6312aa9cd254fa91de73d9b419e181
QIANGWEI WANG <qiangwei.wang@sh.shin-china.com>
2024-12-24 08:14:24 +0000

    20241224 SD010 得意先検収データアップロード

--- sd-s4apps01/salesacceptance/webapp/controller/Main.controller.js
+++ sd-s4apps01/salesacceptance/webapp/controller/Main.controller.js
@@ -19,7 +19,65 @@ sap.ui.define([
 
             _initialize: function () {
                 this._BusyDialog = new BusyDialog();
-                // bind attachment path
+
+                this._UserInfo = sap.ushell.Container.getService("UserInfo");
+                var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                    "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+                });
+                oContextBinding.requestObject().then(function (context) {
+                    var aAccessBtns = [],
+                        aAllAccessBtns = [];
+                    if (context._AssignRole && context._AssignRole.length > 0) {
+                        context._AssignRole.forEach(role => {
+                            aAccessBtns.push(role._UserRoleAccessBtn);
+                        });
+                        aAllAccessBtns = aAccessBtns.flat();
+                    }
+                    if (!aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-View")) {
+                        if (!this.oErrorMessageDialog) {
+                            this.oErrorMessageDialog = new sap.m.Dialog({
+                                type: sap.m.DialogType.Message,
+                                state: "Error",
+                                content: new sap.m.Text({
+                                    text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                                })
+                            });
+                        }
+                        this.oErrorMessageDialog.open();
+                    }
+                    this.getModel("local").setProperty("/authorityCheck", {
+                        button: {
+                            View: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-View"),
+                            Clear: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Clear"),
+                            Check: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Check"),
+                            Create: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Create"),
+                            Overwrite: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Overwrite"),
+                            Delete: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Delete"),
+                            Add: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Add"),
+                            Export: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptance-Export")
+                        },
+                        data: {
+                            PlantSet: context._AssignPlant,
+                            CompanySet: context._AssignCompany,
+                            SalesOrgSet: context._AssignSalesOrg,
+                            PurchOrgSet: context._AssignPurchOrg,
+                            RoleSet: context._AssignRole
+                        }
+                    });
+                }.bind(this), function (oError) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }.bind(this));
 
             },
 
@@ -247,7 +305,7 @@ sap.ui.define([
                 var oTable = this.getView().byId("tableSalesAcceptance");
                 var sPath = oTable.getBindingPath("rows");
                 var aExcelSet = this.getModel("local").getProperty(sPath);
-                
+
                 if (aExcelSet.length === 0) {
                     MessageBox.error(this.getI18nBundle().getText("msgNoDataExport"));
                     return;
@@ -263,7 +321,7 @@ sap.ui.define([
                             label: sLabelText,
                             // 数据类型，即设置excel该列的数据类型
                             type: "string",
-                        
+
                             // 获取数据的绑定路径，即设置excel该列的字段路径
                             property: sTemplatePath,
                             // 获取表格的width属性，即设置excel该列的长度
@@ -281,10 +339,10 @@ sap.ui.define([
                             hierarchyLevel: "level"
                         }
                     },
-                    dataSource: aExcelSet, 
+                    dataSource: aExcelSet,
                     fileName: "Export_" + this.getResourceBundle().getText("title") + formatter.formatDate(new Date()) + ".xlsx" // 文件名，需要加上后缀
                 };
-                
+
                 // 导出excel
                 new Spreadsheet(oSettings).build();
             }
