8d458177a693655d67f105fd6bca899ad71a8ae8
ZHAO WANG <zhao.wang@sh.shin-china.com>
2024-12-24 09:15:13 +0000

    20241224 PP008 ECNレポート 权限检查

--- pp-s4apps01/zecn/webapp/Component.js
+++ pp-s4apps01/zecn/webapp/Component.js
@@ -1,11 +1,19 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+    ["sap/fe/core/AppComponent",
+    "pp/zecn/ext/controller/ListReportExt"
+    ],
+    function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("pp.zecn.Component", {
+            ListReportExt: ListReportExt,
+
             metadata: {
                 manifest: "json"
+            },
+
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
