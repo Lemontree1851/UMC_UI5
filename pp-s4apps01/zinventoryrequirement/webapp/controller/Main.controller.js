884a63c831a825b90d3fb720cc02797c3b8ac76f
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-25 09:27:16 +0000

    PP-032 在庫/所要一覧レポート 权限检查

--- pp-s4apps01/zinventoryrequirement/webapp/controller/Main.controller.js
+++ pp-s4apps01/zinventoryrequirement/webapp/controller/Main.controller.js
@@ -20,6 +20,65 @@ sap.ui.define([
         onInit: function () {
             this._oTable = this.byId("idListTable");
             this._myBusyDialog = new BusyDialog();
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
+        },
+
+        _initialize: function () {
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-View"),
+                        Export: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-Export"),
+                        Summary: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-Summary"),
+                        PurchaseList: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-PurchaseList"),
+                        PurchaseListExport: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryrequirement-PurchaseListExport")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         onSearch: function () {
