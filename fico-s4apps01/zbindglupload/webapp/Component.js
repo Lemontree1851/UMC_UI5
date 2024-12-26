8c075a117b0c863a445d649afe6dafdaeac1c195
siyun <siyun.yao@sh.shin-china.com>
2024-12-26 13:54:41 +0000

    20241226 FICO-020 連結勘定一括登録 权限检查

--- fico-s4apps01/zbindglupload/webapp/Component.js
+++ fico-s4apps01/zbindglupload/webapp/Component.js
@@ -1,11 +1,17 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "fico/zbindglupload/ext/controller/ListReportAuth"
+],
+    function (Component, ListReportAuth) {
         "use strict";
 
         return Component.extend("fico.zbindglupload.Component", {
+            ListReportAuth: ListReportAuth,
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                ListReportAuth.init(this.oModels);
             }
         });
     }
