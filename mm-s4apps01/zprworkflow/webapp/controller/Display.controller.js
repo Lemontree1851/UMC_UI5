844da047512d0292651ee0a919d6173e9d23f9af
siyun <siyun.yao@sh.shin-china.com>
2024-12-26 03:33:24 +0000

    20241226 MM-021 購買申請承認 权限检查

--- mm-s4apps01/zprworkflow/webapp/controller/Display.controller.js
+++ mm-s4apps01/zprworkflow/webapp/controller/Display.controller.js
@@ -1,14 +1,14 @@
 sap.ui.define([
-    "./BaseController",
-    "../model/formatter",
-    "./messages",
+	"./BaseController",
+	"../model/formatter",
+	"./messages",
 	"sap/ui/model/Filter",
 	"sap/m/BusyDialog",
 	"sap/ui/core/Fragment"
-], function(
-    BaseController,
-    formatter,
-    messages,
+], function (
+	BaseController,
+	formatter,
+	messages,
 	Filter,
 	BusyDialog,
 	Fragment
@@ -16,21 +16,65 @@ sap.ui.define([
 	"use strict";
 
 	return BaseController.extend("mm.zprworkflow.controller.Display", {
-        formatter : formatter,
-        onInit: function () {
+		formatter: formatter,
+		onInit: function () {
 			this._LocalData = this.getOwnerComponent().getModel("local");
 			this._oDataModel = this.getOwnerComponent().getModel();
 			this._ResourceBundle = this.getOwnerComponent().getModel("i18n").getResourceBundle();
-            this._BusyDialog = new BusyDialog();
-            var oRouter = this.getRouter();
+			this._BusyDialog = new BusyDialog();
+			var oRouter = this.getRouter();
 			oRouter.getRoute("RouteMain").attachMatched(this._onRouteMatched, this);
 			if (sap.ushell && sap.ushell.Container) {
 				this._UserFullName = sap.ushell.Container.getService("UserInfo").getUser().getFullName();
 				this._UserEmail = sap.ushell.Container.getService("UserInfo").getUser().getEmail();
-				 
+
 			};
-        },
-        onRowActionItemPress : function(oEvent){
+			// *************************************************
+			this.getRouter().getRoute("RouteMain").attachMatched(this._initialize, this);
+		},
+
+		_initialize: function () {
+			var sUser = this._UserFullName;
+			var sEmail = this._UserEmail;
+			var oContextBinding = this.getOwnerComponent().getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+				"$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+			});
+			oContextBinding.requestObject().then(function (context) {
+				var aAccessBtns = [],
+					aAllAccessBtns = [];
+				if (context._AssignRole && context._AssignRole.length > 0) {
+					context._AssignRole.forEach(role => {
+						aAccessBtns.push(role._UserRoleAccessBtn);
+					});
+					aAllAccessBtns = aAccessBtns.flat();
+				}
+				if (!aAllAccessBtns.some(btn => btn.AccessId === "zprworkflow-View")) {
+					if (!this.oErrorMessageDialog) {
+						this.oErrorMessageDialog = new sap.m.Dialog({
+							type: sap.m.DialogType.Message,
+							state: "Error",
+							content: new sap.m.Text({
+								text: this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+							})
+						});
+					}
+					this.oErrorMessageDialog.open();
+				}
+
+			}.bind(this), function (oError) {
+				if (!this.oErrorMessageDialog) {
+					this.oErrorMessageDialog = new sap.m.Dialog({
+						type: sap.m.DialogType.Message,
+						state: "Error",
+						content: new sap.m.Text({
+							text: this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+						})
+					});
+				}
+				this.oErrorMessageDialog.open();
+			}.bind(this));
+		},
+		onRowActionItemPress: function (oEvent) {
 			var oItem, oCtx;
 
 			oItem = oEvent.getSource();
@@ -40,8 +84,8 @@ sap.ui.define([
 			if (!InstanceId) {
 				InstanceId = "00000000-0000-0000-0000-000000000000";
 			}
-			this.getRouter().navTo("PurchaseReq",{
-				contextPath : oCtx.getProperty("UUID"),
+			this.getRouter().navTo("PurchaseReq", {
+				contextPath: oCtx.getProperty("UUID"),
 				contextPrNo: oCtx.getProperty("PrNo"),
 				contextApplyDepart: oCtx.getProperty("ApplyDepart"),
 
@@ -49,8 +93,8 @@ sap.ui.define([
 				contextApplicationId: oCtx.getProperty("ApplicationId"),
 			});
 		},
-        _onRouteMatched : function (oEvent) {
-            this.getView().getModel().resetChanges();
+		_onRouteMatched: function (oEvent) {
+			this.getView().getModel().resetChanges();
 			// var oArgs, oView;
 
 			// oArgs = oEvent.getParameter("arguments");
@@ -76,9 +120,9 @@ sap.ui.define([
 			this._oDataModel.resetChanges();
 			var oFilter = oEvent.getParameter("bindingParams").filters;
 			oFilter.push(new sap.ui.model.Filter("EmailAddress", "EQ", this._UserEmail));
-// 			this._oDataModel.resetChanges();
- 			var oFilter = oEvent.getParameter("bindingParams").filters;
- 			var oNewFilter, aNewFilter = [];
+			// 			this._oDataModel.resetChanges();
+			var oFilter = oEvent.getParameter("bindingParams").filters;
+			var oNewFilter, aNewFilter = [];
 			var oApplyDate = this.byId("idDatePicker").getDateValue();
 			if (oApplyDate) {
 				var year = oApplyDate.getFullYear();
@@ -86,17 +130,16 @@ sap.ui.define([
 				var day = oApplyDate.getDate().toString().padStart(2, '0');
 				var formattedDate = `${year}${month}${day}`;
 				if (oApplyDate) {
-					console.log("ApplyDate",formattedDate)	;
-				    aNewFilter.push(new Filter("ApplyDate", "EQ", formattedDate)); 
+					aNewFilter.push(new Filter("ApplyDate", "EQ", formattedDate));
 				}
 			}
 
- 			oNewFilter = new Filter({
- 				filters:aNewFilter 
- 			});
- 			if (aNewFilter.length > 0) {
+			oNewFilter = new Filter({
+				filters: aNewFilter
+			});
+			if (aNewFilter.length > 0) {
 				oFilter.push(oNewFilter);
- 			}
+			}
 		},
 
 		createPurchseOrder: function (oEvent) {
@@ -120,7 +163,7 @@ sap.ui.define([
 			this.postAction("RejectPRWF", JSON.stringify(aSelectedItems));
 
 		},
-		preparePostBody:function (stextarea) {
+		preparePostBody: function (stextarea) {
 			//var oTextArea = this.byId("textArea1"); // Get the TextArea control by ID
 			//var sValue = oTextArea.getValue(); // Retrieve the value from the TextArea
 			var aData = [];
@@ -133,14 +176,14 @@ sap.ui.define([
 				messages.showError(this._ResourceBundle.getText("msgNoSelect"));
 				return aData;
 			}
-			listItems.forEach(_getData,this); //根据选择的行获取具体的数据
+			listItems.forEach(_getData, this); //根据选择的行获取具体的数据
 			function _getData(iSelected, index) { //sSelected为选中的行
 				let key = oTable.getContextByIndex(iSelected).getPath();
 				let lineData = this._oDataModel.getProperty(key); //根据选中的行获取到ODATA键值，然后再获取到具体属性值
-				lineData.Remark =  stextarea;
+				lineData.Remark = stextarea;
 				lineData.WorkflowId = "purchaserequisition";
 				lineData.UserEmail = this._UserEmail;
-				lineData.UserFullName = this._UserFullName; 
+				lineData.UserFullName = this._UserFullName;
 				lineData.timezone = sTimeZone;
 				let postData = JSON.parse(JSON.stringify(lineData));
 				aData.push(postData);
@@ -152,61 +195,61 @@ sap.ui.define([
 			const offsetMinutes = -date.getTimezoneOffset(); // 与 UTC 的分钟偏移量
 			const hours = Math.floor(offsetMinutes / 60);
 			const minutes = Math.abs(offsetMinutes % 60);
-			
+
 			// 格式化为简短 UTC±HHMM 格式
 			const sign = hours >= 0 ? '+' : '-';
-			const formattedOffset = minutes === 0 
-				? `UTC${sign}${Math.abs(hours)}` 
+			const formattedOffset = minutes === 0
+				? `UTC${sign}${Math.abs(hours)}`
 				: `UTC${sign}${Math.abs(hours)}${minutes}`;
 			return formattedOffset;
 		},
-		removeDuplicates: function(arr) {
+		removeDuplicates: function (arr) {
 			const map = new Map();
 			arr.forEach(item => map.set(item.PrNo));
 			return Array.from(map.values());
 		},
 		postAction: function (sAction, postData) {
 			this._BusyDialog.open();
-            var oModel = this._oDataModel;
-            oModel.callFunction(`/${sAction}`, {
-                method: "POST",
-                // groupId: "myId",//如果设置groupid，会多条一起进入action
-                changeSetId: 1,
-                //建议只传输前端修改的参数，其他字段从后端获取
-                urlParameters: {
-                    Event: sAction,
-                    Zzkey: postData
-                },
-                success: function (oData) {
+			var oModel = this._oDataModel;
+			oModel.callFunction(`/${sAction}`, {
+				method: "POST",
+				// groupId: "myId",//如果设置groupid，会多条一起进入action
+				changeSetId: 1,
+				//建议只传输前端修改的参数，其他字段从后端获取
+				urlParameters: {
+					Event: sAction,
+					Zzkey: postData
+				},
+				success: function (oData) {
 					var aDataKey = Object.getOwnPropertyNames(this._oDataModel.getProperty("/"));
 					for (var i = aDataKey.length - 1; i >= 0; i--) {
-						if (aDataKey[i].slice(0,11) !== "PurchaseReq") {
+						if (aDataKey[i].slice(0, 11) !== "PurchaseReq") {
 							aDataKey.splice(i, 1);
 						}
 					}
-                    let result = JSON.parse(oData[sAction].Zzkey);
-                    result.forEach(function (line) {
-                        aDataKey.forEach(function(key, index){
+					let result = JSON.parse(oData[sAction].Zzkey);
+					result.forEach(function (line) {
+						aDataKey.forEach(function (key, index) {
 							var lineData = this._oDataModel.getProperty("/" + key);
 							if (lineData.UUID.replace(/-/g, '') === this.base64ToHex(line.UUID)) {
 								// lineCount++;
 								this._oDataModel.setProperty("/" + key + "/Type", line.TYPE);
 								this._oDataModel.setProperty("/" + key + "/Message", line.MESSAGE);
 								//this._oDataModel.setProperty("/" + key + "/ResultText", line.RESULTTEXT);
- 
+
 							}
-						},this);
-                    },this);
+						}, this);
+					}, this);
 					this._BusyDialog.close();
-                }.bind(this),
-                error: function (oError) {
-                    this._LocalData.setProperty("/recordCheckSuccessed", false);
-                    messages.showError(messages.parseErrors(oError));
+				}.bind(this),
+				error: function (oError) {
+					this._LocalData.setProperty("/recordCheckSuccessed", false);
+					messages.showError(messages.parseErrors(oError));
 					this._BusyDialog.close();
-                }.bind(this)
-            });
-            // oModel.submitChanges({ groupId: "myId" });
-        },
+				}.bind(this)
+			});
+			// oModel.submitChanges({ groupId: "myId" });
+		},
 
 		base64ToHex: function (base64) {
 			const raw = atob(base64);  // Decode the base64 string
@@ -221,9 +264,9 @@ sap.ui.define([
 			var aSelectedItems = this.preparePostBody();
 			if (aSelectedItems.length === 0) {
 				return;
-			}			
-            if (!this.Dialog) {
-                var oView = this.getView();
+			}
+			if (!this.Dialog) {
+				var oView = this.getView();
 				if (!this.Dialog) {
 					var oView = this.getView();
 					if (!this.Dialog) {
@@ -231,30 +274,30 @@ sap.ui.define([
 							id: oView.getId(),
 							name: "mm.zprworkflow.fragment.Dialog",
 							controller: this
-						}).then(function (oDialog){
+						}).then(function (oDialog) {
 							this.getView().addDependent(oDialog);
 							// oDialog.setModel(oView.getModel());
 							return oDialog;
 						}.bind(this));
 					}
 				}
-				this.Dialog.then(function(oDialog) {
+				this.Dialog.then(function (oDialog) {
 					oDialog.open();
 				}.bind(this));
-            }
-            this.Dialog.then(function(oDialog) {
-                oDialog.open();
+			}
+			this.Dialog.then(function (oDialog) {
+				oDialog.open();
 				var oTextArea = this.byId("textArea1"); // Get the TextArea control by ID
-				oTextArea.setValue(""); 
-            }.bind(this));
-        },
-		onDialogRejectPress: function () {	
+				oTextArea.setValue("");
+			}.bind(this));
+		},
+		onDialogRejectPress: function () {
 			var aSelectedItems = this.preparePostBody();
 			if (aSelectedItems.length === 0) {
 				return;
-			}		
-            if (!this.DialogReject) {
-                var oView = this.getView();
+			}
+			if (!this.DialogReject) {
+				var oView = this.getView();
 				if (!this.DialogReject) {
 					var oView = this.getView();
 					if (!this.DialogReject) {
@@ -262,44 +305,44 @@ sap.ui.define([
 							id: oView.getId(),
 							name: "mm.zprworkflow.fragment.DialogReject",
 							controller: this
-						}).then(function (oDialog){
+						}).then(function (oDialog) {
 							this.getView().addDependent(oDialog);
 							// oDialog.setModel(oView.getModel());
 							return oDialog;
 						}.bind(this));
 					}
 				}
-				this.DialogReject.then(function(oDialog) {
+				this.DialogReject.then(function (oDialog) {
 					oDialog.open();
 				}.bind(this));
-            }
-            this.DialogReject.then(function(oDialog) {
-                oDialog.open();
+			}
+			this.DialogReject.then(function (oDialog) {
+				oDialog.open();
 				var oTextArea = this.byId("textArea"); // Get the TextArea control by ID
-				oTextArea.setValue(""); 
-            }.bind(this));
-        },
-        onDialogClose: function(){
+				oTextArea.setValue("");
+			}.bind(this));
+		},
+		onDialogClose: function () {
 
-            this.byId("AnswerDialogq").close();
-        },
+			this.byId("AnswerDialogq").close();
+		},
 
-        onDialogConfirm: function() {
+		onDialogConfirm: function () {
 			var oTextArea = this.byId("textArea1"); // Get the TextArea control by ID
 			var sValue = oTextArea.getValue(); // Retrieve the value from the TextArea
 			var aSelectedItems = this.preparePostBody(sValue);
 			if (aSelectedItems.length === 0) {
 				return;
 			}
-            this.postAction("AcceptPRWF", JSON.stringify(aSelectedItems));
+			this.postAction("AcceptPRWF", JSON.stringify(aSelectedItems));
 			this.byId("AnswerDialogq").close();
-        },
-		onDialogCloseReject: function(){
+		},
+		onDialogCloseReject: function () {
 
-            this.byId("AnswerDialog").close();
-        },
+			this.byId("AnswerDialog").close();
+		},
 
-        onDialogConfirmReject: function() {
+		onDialogConfirmReject: function () {
 			var oTextArea = this.byId("textArea"); // Get the TextArea control by ID
 			var sValue = oTextArea.getValue(); // Retrieve the value from the TextArea
 			var aSelectedItems = this.preparePostBody(sValue);
@@ -308,12 +351,12 @@ sap.ui.define([
 			}
 			this.postAction("RejectPRWF", JSON.stringify(aSelectedItems));
 			this.byId("AnswerDialog").close();
-        },
-		formatDate: function(date) {
+		},
+		formatDate: function (date) {
 			var year = date.getFullYear();
 			var month = (date.getMonth() + 1).toString().padStart(2, '0'); // Months are 0-based
 			var day = date.getDate().toString().padStart(2, '0');
-			
+
 			return `${year}${month}${day}`;
 		}
 
