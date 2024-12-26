64188b621fd4ac6fac6bb096c684d07e1c3c7a18
siyun <siyun.yao@sh.shin-china.com>
2024-12-26 13:54:06 +0000

    20241226 FICO-018 継続年齢アップデート 权限检查

--- fico-s4apps01/zstockageupload/webapp/Component.js
+++ fico-s4apps01/zstockageupload/webapp/Component.js
@@ -1,11 +1,17 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "fico/zstockageupload/ext/controller/ListReportAuth"
+],
+    function (Component, ListReportAuth) {
         "use strict";
-
+        
         return Component.extend("fico.zstockageupload.Component", {
+            ListReportAuth: ListReportAuth,
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                ListReportAuth.init(this.oModels);
             }
         });
     }
