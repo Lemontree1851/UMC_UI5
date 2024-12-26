e087487834285789a29d301094b09ee225f8581f
YI RAO <yi.rao@sh.shin-china.com>
2024-12-26 05:11:19 +0000

    FICO-019＆026 長期滞在＆低価法レポート 20241226

--- fico-s4apps01/zinventoryaging/webapp/controller/Main.controller.js
+++ fico-s4apps01/zinventoryaging/webapp/controller/Main.controller.js
@@ -1,8 +1,9 @@
 sap.ui.define([
     "../model/formatter",
     "sap/ui/core/mvc/Controller",
+    "sap/ui/core/UIComponent",
     "sap/m/MessageBox"
-], function (formatter,Controller,MessageBox) {
+], function (formatter, Controller, UIComponent, MessageBox) {
     "use strict";
 
     return Controller.extend("fico.zinventoryaging.controller.Main", {
@@ -59,6 +60,85 @@ sap.ui.define([
                 verticalScrolling: false
             });
             // *************************************************
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
+        },
+
+        _initialize: function () {
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getView().getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "zinventoryaging-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getView().getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getOwnerComponent().getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryaging-View"),
+                        ReCalculate: aAllAccessBtns.some(btn => btn.AccessId === "zinventoryaging-Calculate"),
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getView().getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
+        },
+
+        getRouter: function () {
+            return UIComponent.getRouterFor(this);
+        },
+
+        // 点执行按钮后的响应
+        onBeforeRebindTable: function (oEvent) {
+            var aFilters = oEvent.getParameter("bindingParams").filters;
+
+            // 获取 选择字段 的值
+            var oSmartFilterBar = this.byId("idSmartFilterBar");
+            var sCompanyCode = oSmartFilterBar.getFilterData().CompanyCode;
+            var aAuthorityCompanySet = this.getView().getModel("local").getProperty("/authorityCheck/data/CompanySet");
+
+            if (!aAuthorityCompanySet.some(data => data.CompanyCode === sCompanyCode)) {
+                MessageBox.error(this.getView().getModel("i18n").getResourceBundle().getText("noAuthorityCompany", [sCompanyCode]));    
+
+                var oFiltersCompanyCode = aFilters[0].aFilters.find(Filters => Filters.sPath === "CompanyCode");
+                if (oFiltersCompanyCode) {
+                    oFiltersCompanyCode.oValue1 = '';
+                }
+            }
         },
 
         onPressBtn: function (sEvent) {
@@ -72,6 +152,7 @@ sap.ui.define([
                 var sFiscalPeriod = oSmartFilterBar.getFilterData().FiscalPeriod;
                 var sLedger = oSmartFilterBar.getFilterData().Ledger;
                 var sFieldName = '';
+                var aAuthorityCompanySet = this.getView().getModel("local").getProperty("/authorityCheck/data/CompanySet");
 
                 if (!sCompanyCode) {
                     sFieldName = this.getView().getModel("i18n").getResourceBundle().getText("CompanyCode");
@@ -94,6 +175,11 @@ sap.ui.define([
                 if (!sLedger) {
                     sFieldName = this.getView().getModel("i18n").getResourceBundle().getText("Ledger");
                     MessageBox.error(this.getView().getModel("i18n").getResourceBundle().getText("FilterMandatory",[sFieldName]));
+                    return;
+                }
+
+                if (!aAuthorityCompanySet.some(data => data.CompanyCode === sCompanyCode)) {
+                    MessageBox.error(this.getView().getModel("i18n").getResourceBundle().getText("noAuthorityCompany", [sCompanyCode]));     
                     return;
                 }
 
@@ -240,12 +326,6 @@ sap.ui.define([
         // 点执行按钮后的响应
         onSearch: function (oEvent) {
            
-        },
-
-        // 点执行按钮后的响应
-        onBeforeRebindTable: function (oEvent) {
-            // 根据选择框，添加过滤条件传值到后端
-
         },
 
         // 点导出按钮后的响应
--- fico-s4apps01/zinventoryaging/webapp/model/models.js
+++ fico-s4apps01/zinventoryaging/webapp/model/models.js
@@ -13,6 +13,18 @@ function (JSONModel, Device) {
             var oModel = new JSONModel(Device);
             oModel.setDefaultBindingMode("OneWay");
             return oModel;
+        },
+
+        // Create local Model
+        createLocalModel: function () {
+            var oModel = new JSONModel({
+                selectIndex: 0,
+                std_mode: "display",
+                tab_mode: "display",
+                dateValue: new Date(),
+                detailSet: []
+            });
+            return oModel;
         }
     };
 
