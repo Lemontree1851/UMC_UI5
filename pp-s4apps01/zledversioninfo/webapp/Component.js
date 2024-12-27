36dfa8ab625cd2ddd927223ef87b79beb80417ac
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-27 07:07:21 +0000

    PP-034 LED生産品目製造バージョン情報管理 权限检查

--- pp-s4apps01/zledversioninfo/webapp/Component.js
+++ pp-s4apps01/zledversioninfo/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "pp/zledversioninfo/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("pp.zledversioninfo.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("pp.zledversioninfo.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            this.ListReportExt.getAuthorityData(this.oModels);
+        }
+    });
+});
