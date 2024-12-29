884136175916c3502aca037966d0b83ae2654567
KAIQIANG SU <kaiqiang.su@sh.shin-china.com>
2024-12-29 11:44:53 +0000

    20241229 BI-002 日別在庫金額照会 权限检查

--- fico-s4apps01/zdaystocktrans/webapp/Component.js
+++ fico-s4apps01/zdaystocktrans/webapp/Component.js
@@ -1,11 +1,17 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+    ["sap/fe/core/AppComponent",
+     "fico/zdaystocktrans/ext/controller/ListReportAuth"
+    ],
+    function (Component,ListReportAuth) {
         "use strict";
 
         return Component.extend("fico.zdaystocktrans.Component", {
+            ListReportAuth: ListReportAuth,
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                this.ListReportAuth.init(this.oModels);
             }
         });
     }
