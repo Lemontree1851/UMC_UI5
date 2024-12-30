dc92a4492983b89cfb2de64267ebe1e0e29431db
YI RAO <yi.rao@sh.shin-china.com>
2024-12-30 05:24:23 +0000

    PP-035 製造指図一括発行 20241230

--- pp-s4apps01/zproductionorderrelease/webapp/Component.js
+++ pp-s4apps01/zproductionorderrelease/webapp/Component.js
@@ -1,11 +1,19 @@
 sap.ui.define(
-    ["sap/suite/ui/generic/template/lib/AppComponent"],
-    function (Component) {
+    ["sap/suite/ui/generic/template/lib/AppComponent",
+     "pp/zproductionorderrelease/ext/controller/ListReportAuthCheck"
+    ],
+    function (Component, ListReportAuthCheck) {
         "use strict";
 
         return Component.extend("pp.zproductionorderrelease.Component", {
+            ListReportAuthCheck: ListReportAuthCheck,
+
             metadata: {
                 manifest: "json"
+            },
+
+            onAfterRendering: function () {
+                ListReportAuthCheck.init(this.oModels);
             }
         });
     }
