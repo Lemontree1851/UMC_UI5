fb52a6dcbd4207e2b043fd088d5f8ce2a1990c98
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-30 03:21:45 +0000

    PP-011 OFとSO比較照会 权限检查

--- pp-s4apps01/ofsocomparison/webapp/controller/Main.controller.js
+++ pp-s4apps01/ofsocomparison/webapp/controller/Main.controller.js
@@ -9,22 +9,78 @@ sap.ui.define([
 	"sap/ui/model/FilterOperator",
 	"pp/ofsocomparison/model/formatter",
 	'sap/ui/export/Spreadsheet',
-],
-function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterOperator, formatter,Spreadsheet) {
-    "use strict";
+], function (Base, Column, Text, MessageBox, BusyDialog, MessageToast, Filter, FilterOperator, formatter, Spreadsheet) {
+	"use strict";
 
-    return Base.extend("pp.ofsocomparison.controller.Main", {
+	return Base.extend("pp.ofsocomparison.controller.Main", {
 
-        formatter: formatter,
+		formatter: formatter,
 
-        onInit: function () {
+		onInit: function () {
 			this._BusyDialog = new BusyDialog();
+			this._UserInfo = sap.ushell.Container.getService("UserInfo");
+			this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
+		},
 
-        },
+		_initialize: function () {
+			var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+			var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+			sEmail = "xinlei.xu@sh.shin-china.com";
+			debugger;
+			var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+				"$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+			});
+			oContextBinding.requestObject().then(function (context) {
+				var aAccessBtns = [],
+					aAllAccessBtns = [];
+				if (context._AssignRole && context._AssignRole.length > 0) {
+					context._AssignRole.forEach(role => {
+						aAccessBtns.push(role._UserRoleAccessBtn);
+					});
+					aAllAccessBtns = aAccessBtns.flat();
+				}
+				if (!aAllAccessBtns.some(btn => btn.AccessId === "ofsocomparison-View")) {
+					if (!this.oErrorMessageDialog) {
+						this.oErrorMessageDialog = new sap.m.Dialog({
+							type: sap.m.DialogType.Message,
+							state: "Error",
+							content: new sap.m.Text({
+								text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+							})
+						});
+					}
+					this.oErrorMessageDialog.open();
+				}
+				this.getModel("local").setProperty("/authorityCheck", {
+					button: {
+						View: aAllAccessBtns.some(btn => btn.AccessId === "ofsocomparison-View"),
+						Export: aAllAccessBtns.some(btn => btn.AccessId === "ofsocomparison-Export")
+					},
+					data: {
+						PlantSet: context._AssignPlant,
+						CompanySet: context._AssignCompany,
+						SalesOrgSet: context._AssignSalesOrg,
+						PurchOrgSet: context._AssignPurchOrg,
+						RoleSet: context._AssignRole
+					}
+				});
+			}.bind(this), function (oError) {
+				if (!this.oErrorMessageDialog) {
+					this.oErrorMessageDialog = new sap.m.Dialog({
+						type: sap.m.DialogType.Message,
+						state: "Error",
+						content: new sap.m.Text({
+							text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+						})
+					});
+				}
+				this.oErrorMessageDialog.open();
+			}.bind(this));
+		},
 
 		onSearch: function () {
 
-            var that = this;
+			var that = this;
 
 			this.getModel().resetChanges();
 			var oSearchBar = this.byId("idSmartFilterBar1");
@@ -39,27 +95,27 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 			var sContents = this.getModel("local").getProperty("/Contents");
 			if (sContents) {
 				aFilters.push(new Filter("Contents", FilterOperator.EQ, sContents));
-			}    
+			}
 
 			var oDateRange1 = that.byId("idDateRangeSelection1");
-			if(oDateRange1.getFrom()){
-				var splitStart1 = `${oDateRange1.getFrom().getFullYear()}${(oDateRange1.getFrom().getMonth() + 1).toString().padStart(2,"0")}`;
-				var splitEnd1 = `${oDateRange1.getTo().getFullYear()}${(oDateRange1.getTo().getMonth() + 1).toString().padStart(2,"0")}`;
+			if (oDateRange1.getFrom()) {
+				var splitStart1 = `${oDateRange1.getFrom().getFullYear()}${(oDateRange1.getFrom().getMonth() + 1).toString().padStart(2, "0")}`;
+				var splitEnd1 = `${oDateRange1.getTo().getFullYear()}${(oDateRange1.getTo().getMonth() + 1).toString().padStart(2, "0")}`;
 			}
 
 			if (splitStart1) {
-				aFilters.push(new Filter("Duration", FilterOperator.BT,splitStart1,splitEnd1));
-			}  
+				aFilters.push(new Filter("Duration", FilterOperator.BT, splitStart1, splitEnd1));
+			}
 
 			var oDateRange2 = that.byId("idDateRangeSelection2");
-			if(oDateRange2.getFrom()){
-				var splitStart2 = `${oDateRange2.getFrom().getFullYear()}${(oDateRange2.getFrom().getMonth() + 1).toString().padStart(2,"0")}${oDateRange2.getFrom().getDate().toString().padStart(2,"0")}`;
-				var splitEnd2 = `${oDateRange2.getTo().getFullYear()}${(oDateRange2.getTo().getMonth() + 1).toString().padStart(2,"0")}${oDateRange2.getTo().getDate().toString().padStart(2,"0")}`;			
+			if (oDateRange2.getFrom()) {
+				var splitStart2 = `${oDateRange2.getFrom().getFullYear()}${(oDateRange2.getFrom().getMonth() + 1).toString().padStart(2, "0")}${oDateRange2.getFrom().getDate().toString().padStart(2, "0")}`;
+				var splitEnd2 = `${oDateRange2.getTo().getFullYear()}${(oDateRange2.getTo().getMonth() + 1).toString().padStart(2, "0")}${oDateRange2.getTo().getDate().toString().padStart(2, "0")}`;
 			}
-			
+
 			if (splitStart2) {
-				aFilters.push(new Filter("CREATED_AT", FilterOperator.BT,splitStart2,splitEnd2));
-			}   
+				aFilters.push(new Filter("CREATED_AT", FilterOperator.BT, splitStart2, splitEnd2));
+			}
 
 			var oFilterData = oSearchBar.getFilterData();
 			this.getTableContent(aFilters, oFilterData, oTable);
@@ -75,20 +131,20 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 
 			// Promise.all([this.readData(aFilters)]).then((results) => {
 			this._BusyDialog.open();
-			Promise.all([this.readData(aFilters)]).then((results) => {	
+			Promise.all([this.readData(aFilters)]).then((results) => {
 				if (results[0].results.length > 0) {
 					that.getModel("local").setProperty("/data", results[0].results);
 					that.buildListResultUITable(oTable, results[0].results[0]);
 					this._BusyDialog.close();
-                } else {
+				} else {
 					this._BusyDialog.close();
-                    MessageBox.error("対象データが無いです。");
+					MessageBox.error("対象データが無いです。");
 					that.getModel("local").setProperty("/data", results[0].results);
-					that.buildListResultUITable(oTable, results[0].results[0]);	
+					that.buildListResultUITable(oTable, results[0].results[0]);
 
 
-					
-                }	
+
+				}
 			}).catch((error) => {
 				MessageBox.error(error);
 			}).finally(() => {
@@ -165,7 +221,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					width: "10rem"
 				}));
 
-				if (titleVariable.PeriodT1){
+				if (titleVariable.PeriodT1) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT1,
 						template: new Text({
@@ -175,38 +231,38 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT2){
+				if (titleVariable.PeriodT2) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT2,
 						template: new Text({
 							text: "{local>Period2}"
 						}),
 						width: "10rem"
-					}));	
+					}));
 				}
 
-				if (titleVariable.PeriodT3){	
+				if (titleVariable.PeriodT3) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT3,
 						template: new Text({
 							text: "{local>Period3}"
 						}),
 						width: "10rem"
-					}));		
+					}));
 				}
 
-				if (titleVariable.PeriodT4){
+				if (titleVariable.PeriodT4) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT4,
 						template: new Text({
 							text: "{local>Period4}"
 						}),
 						width: "10rem"
-					}));	
+					}));
 				}
 
-				if (titleVariable.PeriodT5){
-				
+				if (titleVariable.PeriodT5) {
+
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT5,
 						template: new Text({
@@ -216,7 +272,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT6){
+				if (titleVariable.PeriodT6) {
 
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT6,
@@ -227,7 +283,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT7){
+				if (titleVariable.PeriodT7) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT7,
 						template: new Text({
@@ -237,7 +293,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT8){
+				if (titleVariable.PeriodT8) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT8,
 						template: new Text({
@@ -247,7 +303,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT9){
+				if (titleVariable.PeriodT9) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT9,
 						template: new Text({
@@ -257,7 +313,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT10){
+				if (titleVariable.PeriodT10) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT10,
 						template: new Text({
@@ -267,7 +323,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT11){
+				if (titleVariable.PeriodT11) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT11,
 						template: new Text({
@@ -277,7 +333,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT12){
+				if (titleVariable.PeriodT12) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT12,
 						template: new Text({
@@ -287,7 +343,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT13){
+				if (titleVariable.PeriodT13) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT13,
 						template: new Text({
@@ -297,7 +353,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT14){
+				if (titleVariable.PeriodT14) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT14,
 						template: new Text({
@@ -307,7 +363,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT15){
+				if (titleVariable.PeriodT15) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT15,
 						template: new Text({
@@ -317,7 +373,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT16){
+				if (titleVariable.PeriodT16) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT16,
 						template: new Text({
@@ -327,7 +383,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT17){
+				if (titleVariable.PeriodT17) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT17,
 						template: new Text({
@@ -337,7 +393,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT18){
+				if (titleVariable.PeriodT18) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT18,
 						template: new Text({
@@ -346,7 +402,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 						width: "10rem"
 					}));
 				}
-				if (titleVariable.PeriodT19){
+				if (titleVariable.PeriodT19) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT19,
 						template: new Text({
@@ -356,7 +412,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT20){
+				if (titleVariable.PeriodT20) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT20,
 						template: new Text({
@@ -366,7 +422,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT21){
+				if (titleVariable.PeriodT21) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT21,
 						template: new Text({
@@ -376,7 +432,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT22){
+				if (titleVariable.PeriodT22) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT22,
 						template: new Text({
@@ -386,7 +442,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT23){
+				if (titleVariable.PeriodT23) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT23,
 						template: new Text({
@@ -396,7 +452,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT24){
+				if (titleVariable.PeriodT24) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT24,
 						template: new Text({
@@ -406,7 +462,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT25){
+				if (titleVariable.PeriodT25) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT25,
 						template: new Text({
@@ -416,7 +472,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT26){
+				if (titleVariable.PeriodT26) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT26,
 						template: new Text({
@@ -426,7 +482,8 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT27){v
+				if (titleVariable.PeriodT27) {
+					v
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT27,
 						template: new Text({
@@ -436,7 +493,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT28){
+				if (titleVariable.PeriodT28) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT28,
 						template: new Text({
@@ -446,7 +503,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT29){
+				if (titleVariable.PeriodT29) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT29,
 						template: new Text({
@@ -456,7 +513,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT30){
+				if (titleVariable.PeriodT30) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT30,
 						template: new Text({
@@ -466,7 +523,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT31){
+				if (titleVariable.PeriodT31) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT31,
 						template: new Text({
@@ -476,7 +533,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT32){
+				if (titleVariable.PeriodT32) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT32,
 						template: new Text({
@@ -486,7 +543,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT33){
+				if (titleVariable.PeriodT33) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT33,
 						template: new Text({
@@ -496,7 +553,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT34){
+				if (titleVariable.PeriodT34) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT34,
 						template: new Text({
@@ -506,7 +563,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT35){
+				if (titleVariable.PeriodT35) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT35,
 						template: new Text({
@@ -516,7 +573,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 					}));
 				}
 
-				if (titleVariable.PeriodT36){
+				if (titleVariable.PeriodT36) {
 					oTable.addColumn(new Column({
 						label: titleVariable.PeriodT36,
 						template: new Text({
@@ -525,7 +582,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 						width: "10rem"
 					}));
 				}
-		    }
+			}
 
 		},
 
@@ -539,7 +596,7 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 				aCols.push({
 					label: column.getLabel().getText(),
 					property: column.getTemplate().getBindingInfo("text").parts[0].path,
-					scale : 0
+					scale: 0
 				});
 			}
 			oSettings = {
@@ -568,5 +625,5 @@ function (Base, Column, Text,MessageBox,BusyDialog,MessageToast, Filter, FilterO
 
 
 
-    });
+	});
 });
