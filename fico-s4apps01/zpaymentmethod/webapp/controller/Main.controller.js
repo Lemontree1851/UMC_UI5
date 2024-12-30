26f13428372c2fd40964a80125de1bdcf9d509e8
siyun <siyun.yao@sh.shin-china.com>
2024-12-30 03:45:53 +0000

    20241230 FICO-015 支払方法変更 权限检查

--- fico-s4apps01/zpaymentmethod/webapp/controller/Main.controller.js
+++ fico-s4apps01/zpaymentmethod/webapp/controller/Main.controller.js
@@ -26,6 +26,7 @@ sap.ui.define([
         formatter: formatter,
 
         onInit: function () {
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
             this.getRouter().getRoute("Main").attachMatched(this._initialize, this);
             this.getOwnerComponent().getModel("local").setProperty("/headSet", {});
             // 设置默认日期范围
@@ -47,6 +48,60 @@ sap.ui.define([
 
         _initialize: function () {
             this._BusyDialog = new BusyDialog();
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getOwnerComponent().getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getOwnerComponent().getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-View"),
+                        TestExcute: aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-TestExcute"),
+                        Excute: aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-Excute"),
+                        ExcuteWithJob: aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-ExcuteWithJob"),
+                        Export: aAllAccessBtns.some(btn => btn.AccessId === "zpaymentmethod-Export")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getOwnerComponent().getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         onFileChange: function (oEvent) {
