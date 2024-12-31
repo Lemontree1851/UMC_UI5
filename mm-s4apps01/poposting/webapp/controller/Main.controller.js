845316c8ac97629bdea0ae310262ac32c6e3c679
XINLEI XU <xinlei.xu@sh.shin-china.com>
2024-12-31 06:59:01 +0000

    MM-020 購買発注入庫実績一括登録 权限检查

--- mm-s4apps01/poposting/webapp/controller/Main.controller.js
+++ mm-s4apps01/poposting/webapp/controller/Main.controller.js
@@ -17,6 +17,60 @@ sap.ui.define([
 
         _initialize: function () {
             this._BusyDialog = new BusyDialog();
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "poposting-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "poposting-View"),
+                        Clear: aAllAccessBtns.some(btn => btn.AccessId === "poposting-Clear"),
+                        Excute: aAllAccessBtns.some(btn => btn.AccessId === "poposting-Excute"),
+                        Export: aAllAccessBtns.some(btn => btn.AccessId === "poposting-Export")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
         },
 
         onFileChange: function (oEvent) {
@@ -53,7 +107,6 @@ sap.ui.define([
                         "Batch": aSheetData[i]["BATCH"] === undefined ? "" : aSheetData[i]["BATCH"],
                         "Plant": aSheetData[i]["PLANT"] === undefined ? "" : aSheetData[i]["PLANT"],
                         "StorageLocation": aSheetData[i]["STORAGELOCATION"] === undefined ? "" : aSheetData[i]["STORAGELOCATION"]
-
                     };
                     aExcelSet.push(item);
                 }
@@ -133,9 +186,9 @@ sap.ui.define([
                                         aExcelSet[index].Status = element.STATUS;
                                         aExcelSet[index].Message = element.MESSAGE;
                                         aExcelSet[index].DocumentDate = element.DOCUMENTDATE,
-                                        aExcelSet[index].PostingDate = element.POSTINGDATE,
-                                        aExcelSet[index].Plant = element.PLANT,
-                                        aExcelSet[index].StorageLocation = element.STORAGELOCATION
+                                            aExcelSet[index].PostingDate = element.POSTINGDATE,
+                                            aExcelSet[index].Plant = element.PLANT,
+                                            aExcelSet[index].StorageLocation = element.STORAGELOCATION
                                         aExcelSet[index].MaterialDocument = element.MATERIALDOCUMENT
                                         aExcelSet[index].MaterialDocumentYear = element.MATERIALDOCUMENTYEAR
                                         aExcelSet[index].MaterialDocumentItem = element.MATERIALDOCUMENTITEM
