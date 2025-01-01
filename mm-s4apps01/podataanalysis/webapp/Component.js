febd814d0f1afc9255b5bb9a3c10d8c90a8383f5
ZHAO WANG <zhao.wang@sh.shin-china.com>
2025-01-01 14:47:35 +0000

    MM-001 PO状況分析レポート 权限检查

--- mm-s4apps01/podataanalysis/webapp/Component.js
+++ mm-s4apps01/podataanalysis/webapp/Component.js
@@ -1,11 +1,20 @@
 sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+    ["sap/fe/core/AppComponent",
+     "mm/podataanalysis/ext/controller/ListReportExt"
+    ],
+
+    function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("mm.podataanalysis.Component", {
+
+            ListReportExt: ListReportExt,
             metadata: {
                 manifest: "json"
+            },
+
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
