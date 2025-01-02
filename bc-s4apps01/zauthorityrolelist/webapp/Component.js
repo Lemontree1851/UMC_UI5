3dcf1fc5ca96c862ef197d66df5895106543ad7c
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-01-02 07:16:30 +0000

    BC-006 ロール権限一覧 权限检查

--- bc-s4apps01/zauthorityrolelist/webapp/Component.js
+++ bc-s4apps01/zauthorityrolelist/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "bc/zauthorityrolelist/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("bc.zauthorityrolelist.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("bc.zauthorityrolelist.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            this.ListReportExt.getAuthorityData(this.oModels);
+        }
+    });
+});
