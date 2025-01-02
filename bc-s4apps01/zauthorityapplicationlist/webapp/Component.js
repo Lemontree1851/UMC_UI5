236cac543e3c64b87d492668e4d0a567a7dacefe
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-01-02 07:16:04 +0000

    BC-007 機能権限定義 权限检查

--- bc-s4apps01/zauthorityapplicationlist/webapp/Component.js
+++ bc-s4apps01/zauthorityapplicationlist/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "bc/zauthorityapplicationlist/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("bc.zauthorityapplicationlist.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("bc.zauthorityapplicationlist.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            this.ListReportExt.getAuthorityData(this.oModels);
+        }
+    });
+});
