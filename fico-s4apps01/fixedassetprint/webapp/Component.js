42003fab1dc9a9a4c47fd9e4d5d64aaf940eb3f4
siyun <siyun.yao@sh.shin-china.com>
2025-01-02 03:17:16 +0000

    20250102 FICO-009 权限校验

--- fico-s4apps01/fixedassetprint/webapp/Component.js
+++ fico-s4apps01/fixedassetprint/webapp/Component.js
@@ -1,11 +1,17 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "fico/fixedassetprint/ext/controller/ListReportExt"
+    ],
+    function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("fico.fixedassetprint.Component", {
+            ListReportExt: ListReportExt,
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
