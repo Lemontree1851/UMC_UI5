4734b40660f358e8efc65f51f2eb129b8ed382d2
siyun <siyun.yao@sh.shin-china.com>
2025-01-02 02:48:00 +0000

    20250102 BI-001 权限校验

--- fico-s4apps01/zmfgorder/webapp/Component.js
+++ fico-s4apps01/zmfgorder/webapp/Component.js
@@ -1,11 +1,17 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define( [
+    "sap/fe/core/AppComponent",
+    "fico/zmfgorder/ext/controller/ListReportExt"
+    ],
+    function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("fico.zmfgorder.Component", {
+            ListReportExt: ListReportExt,
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
