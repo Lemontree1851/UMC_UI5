1c75fe20f8ba2dfee046bbb98a86ba52a1777fb4
zoukun-shin <kun.zou@sh.shin-china.com>
2025-01-02 10:47:46 +0000

    20250102 OF分割とPIR登録 权限检查

--- pp-s4apps01/ofpartition/webapp/controller/Main.controller.js
+++ pp-s4apps01/ofpartition/webapp/controller/Main.controller.js
@@ -1,12 +1,69 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "./Base"
 ],
-function (Controller) {
+function (Base) {
     "use strict";
 
-    return Controller.extend("pp.ofpartition.controller.Main", {
+    return Base.extend("pp.ofpartition.controller.Main", {
         onInit: function () {
-
-        }
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            this.getRouter().getRoute("RouteMain").attachMatched(this._initialize, this);
+        },
+        _initialize: function () {
+            var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+            var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+            var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                this.getModel("local").setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-View"),
+                        Check: aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-Check"),
+                        Create: aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-Create"),
+                        Excute: aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-Excute"),
+                        Export: aAllAccessBtns.some(btn => btn.AccessId === "ofpartition-Export"),
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
+        },
     });
 });
