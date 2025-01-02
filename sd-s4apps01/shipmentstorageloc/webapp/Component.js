a7fe3ba2ea235549227ebc29db88d576b9698004
zoukun-shin <kun.zou@sh.shin-china.com>
2025-01-02 10:50:04 +0000

    20250102 出荷保管場所決定マスタ 权限检查

--- sd-s4apps01/shipmentstorageloc/webapp/Component.js
+++ sd-s4apps01/shipmentstorageloc/webapp/Component.js
@@ -1,11 +1,17 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "sd/shipmentstorageloc/ext/controller/ListReportExt"
+    ],
+
+    function (Component, ListReportExt) {
         "use strict";
 
         return Component.extend("sd.shipmentstorageloc.Component", {
             metadata: {
                 manifest: "json"
+            },
+            onAfterRendering: function () {
+                ListReportExt.init(this.oModels);
             }
         });
     }
