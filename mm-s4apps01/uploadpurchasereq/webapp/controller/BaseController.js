76969db206fe5081ccc137552080658495b7dd9d
zoukun-shin <kun.zou@sh.shin-china.com>
2025-01-02 05:40:39 +0000

    20250102 購買申請登録とファイル添付 フィールドの追加

--- mm-s4apps01/uploadpurchasereq/webapp/controller/BaseController.js
+++ mm-s4apps01/uploadpurchasereq/webapp/controller/BaseController.js
@@ -18,6 +18,31 @@ sap.ui.define([
 			return UIComponent.getRouterFor(this);
 		},
 
+		getModel: function (sName) {
+            return this.getView().getModel(sName);
+        },
+
+        /**
+         * Convenience method for setting the view model in every controller of the application.
+         * @public
+         * @param {sap.ui.model.Model} oModel the model instance
+         * @param {string} sName the model name
+         * @returns {sap.ui.mvc.View} the view instance
+         */
+        setModel: function (oModel, sName) {
+            return this.getView().setModel(oModel, sName);
+        },
+
+        /**
+         * Convenience method for getting the resource bundle.
+         * @public
+         * @returns {sap.ui.model.resource.ResourceModel} the resourceModel of the component
+         */
+        getResourceBundle: function () {
+            return this.getOwnerComponent().getModel("i18n").getResourceBundle();
+        },
+
+
 		onNavBack: function () {
 			var oHistory, sPreviousHash;
 
--- mm-s4apps01/uploadpurchasereq/webapp/controller/Upload.controller.js
+++ mm-s4apps01/uploadpurchasereq/webapp/controller/Upload.controller.js
@@ -1,16 +1,16 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "./BaseController",
     "../model/formatter",
     "./messages",
     "../util/xlsx",
     "sap/m/BusyDialog",
     "sap/ui/export/Spreadsheet"
 ], function (
-    Controller, formatter, messages, xlsx, BusyDialog, Spreadsheet
+    BaseController, formatter, messages, xlsx, BusyDialog, Spreadsheet
 ) {
     "use strict";
 
-    return Controller.extend("mm.uploadpurchasereq.controller.Upload", {
+    return BaseController.extend("mm.uploadpurchasereq.controller.Upload", {
         formatter: formatter,
         onInit: function () {
             this._LocalData = this.getOwnerComponent().getModel("local");
@@ -25,7 +25,69 @@ sap.ui.define([
                 "00" + "',DocumentInfoRecordDocPart='" + "000" + "')";
             oUploadSet.bindElement(sPath);
 
+            this._UserInfo = sap.ushell.Container.getService("UserInfo");
+            this.getRouter().getRoute("RouteMain").attachMatched(this._initialize, this);
+
         },
+
+        // _initialize: function () {
+        //     var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+        //     var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+        //     var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+        //         "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+        //     });
+        //     oContextBinding.requestObject().then(function (context) {
+        //         var aAccessBtns = [],
+        //             aAllAccessBtns = [];
+        //         if (context._AssignRole && context._AssignRole.length > 0) {
+        //             context._AssignRole.forEach(role => {
+        //                 aAccessBtns.push(role._UserRoleAccessBtn);
+        //             });
+        //             aAllAccessBtns = aAccessBtns.flat();
+        //         }
+        //         if (!aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-View")) {
+        //             if (!this.oErrorMessageDialog) {
+        //                 this.oErrorMessageDialog = new sap.m.Dialog({
+        //                     type: sap.m.DialogType.Message,
+        //                     state: "Error",
+        //                     content: new sap.m.Text({
+        //                         text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+        //                     })
+        //                 });
+        //             }
+        //             this.oErrorMessageDialog.open();
+        //         }
+        //         this.getModel("local").setProperty("/authorityCheck", {
+        //             button: {
+        //                 View: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-View"),
+        //                 Upload: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-Upload"),
+        //                 Check: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-Check"),
+        //                 Excute: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-Excute"),
+        //                 POLink: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-POLink"),
+        //                 PRApproveReturn: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-PRApproveReturn"),
+        //                 PRApproveSend: aAllAccessBtns.some(btn => btn.AccessId === "uploadpurchasereq-PRApproveSend"),
+        //             },
+        //             data: {
+        //                 PlantSet: context._AssignPlant,
+        //                 CompanySet: context._AssignCompany,
+        //                 SalesOrgSet: context._AssignSalesOrg,
+        //                 PurchOrgSet: context._AssignPurchOrg,
+        //                 RoleSet: context._AssignRole
+        //             }
+        //         });
+        //     }.bind(this), function (oError) {
+        //         if (!this.oErrorMessageDialog) {
+        //             this.oErrorMessageDialog = new sap.m.Dialog({
+        //                 type: sap.m.DialogType.Message,
+        //                 state: "Error",
+        //                 content: new sap.m.Text({
+        //                     text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+        //                 })
+        //             });
+        //         }
+        //         this.oErrorMessageDialog.open();
+        //     }.bind(this));
+        // },
 
         getMediaUrl: function (sUrlString) {
             if (sUrlString) {
@@ -111,6 +173,7 @@ sap.ui.define([
                         GlAccount: aSheet1[i]["GlAccount"] || "",
                         CostCenter: aSheet1[i]["CostCenter"] || "",
                         WbsElemnt: aSheet1[i]["WbsElemnt"] || "",
+                        OrderID: aSheet1[i]["OrderID"] || "",
                         AssetNo: aSheet1[i]["AssetNo"] || "",
                         Tax: aSheet1[i]["Tax"] || "",
                         ItemText: aSheet1[i]["ItemText"] || "",
