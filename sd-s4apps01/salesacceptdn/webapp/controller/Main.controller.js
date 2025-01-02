b3c2b550a1078335ed6bb3b121a08d601b8b4d6a
zoukun-shin <kun.zou@sh.shin-china.com>
2025-01-02 10:49:43 +0000

    20250102 TWX21検収データ取込み機能 权限检查

--- sd-s4apps01/salesacceptdn/webapp/controller/Main.controller.js
+++ sd-s4apps01/salesacceptdn/webapp/controller/Main.controller.js
@@ -1,5 +1,5 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller",
+    "./Base",
     "../model/formatter",
     "./messages",
     "../util/xlsx",
@@ -8,7 +8,7 @@ sap.ui.define([
     "sap/ui/model/Filter",
     "sap/ui/model/Sorter",
 ],
-    function (Controller,
+    function (Base,
         formatter,
         messages,
         xlsx,
@@ -33,7 +33,7 @@ sap.ui.define([
                 items: ["Customer", "OrganizationBPName1"]
             }],
         ]);
-        return Controller.extend("sd.salesacceptdn.controller.Main", {
+        return Base.extend("sd.salesacceptdn.controller.Main", {
             formatter: formatter,
             onInit: function () {
                 this._LocalData = this.getOwnerComponent().getModel("local");
@@ -47,8 +47,65 @@ sap.ui.define([
                     "',DocumentInfoRecordDocNumber='" + "10000000000" + "',DocumentInfoRecordDocVersion='" +
                     "00" + "',DocumentInfoRecordDocPart='" + "000" + "')";
                 oUploadSet.bindElement(sPath);
+
+                this._UserInfo = sap.ushell.Container.getService("UserInfo");
+                this.getRouter().getRoute("RouteMain").attachMatched(this._initialize, this);
             },
 
+            _initialize: function () {
+                var sUser = this._UserInfo.getFullName() === undefined ? "" : this._UserInfo.getFullName();
+                var sEmail = this._UserInfo.getEmail() === undefined ? "" : this._UserInfo.getEmail();
+                sUser="xinlei.xu";
+                sEmail="xinlei.xu@sh.shin-china.com";
+                var oContextBinding = this.getModel("Authority").bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                    "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+                });
+                oContextBinding.requestObject().then(function (context) {
+                    var aAccessBtns = [],
+                        aAllAccessBtns = [];
+                    if (context._AssignRole && context._AssignRole.length > 0) {
+                        context._AssignRole.forEach(role => {
+                            aAccessBtns.push(role._UserRoleAccessBtn);
+                        });
+                        aAllAccessBtns = aAccessBtns.flat();
+                    }
+                    if (!aAllAccessBtns.some(btn => btn.AccessId === "salesacceptdn-View")) {
+                        if (!this.oErrorMessageDialog) {
+                            this.oErrorMessageDialog = new sap.m.Dialog({
+                                type: sap.m.DialogType.Message,
+                                state: "Error",
+                                content: new sap.m.Text({
+                                    text: this.getModel("i18n").getResourceBundle().getText("noAuthorityView", [sUser])
+                                })
+                            });
+                        }
+                        this.oErrorMessageDialog.open();
+                    }
+                    this.getModel("local").setProperty("/authorityCheck", {
+                        button: {
+                            View: aAllAccessBtns.some(btn => btn.AccessId === "salesacceptdn-View"),
+                        },
+                        data: {
+                            PlantSet: context._AssignPlant,
+                            CompanySet: context._AssignCompany,
+                            SalesOrgSet: context._AssignSalesOrg,
+                            PurchOrgSet: context._AssignPurchOrg,
+                            RoleSet: context._AssignRole
+                        }
+                    });
+                }.bind(this), function (oError) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: this.getModel("i18n").getResourceBundle().getText("getAuthorityFailed")
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }.bind(this));
+            },
 
             getMediaUrl: function (sUrlString) {
                 if (sUrlString) {
