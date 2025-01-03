6ad53f0c2eda0696d8850503301b813c3774b5df
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-01-03 08:26:48 +0000

    BC-003 一括アップロード機能実行 权限检查

--- bc-s4apps01/zdtimpfiles/webapp/Component.js
+++ bc-s4apps01/zdtimpfiles/webapp/Component.js
@@ -1,12 +1,19 @@
-sap.ui.define(
-    ["sap/fe/core/AppComponent"],
-    function (Component) {
-        "use strict";
+sap.ui.define([
+    "sap/fe/core/AppComponent",
+    "bc/zdtimpfiles/ext/controller/ListReportExt"
+], function (Component, ListReportExt) {
+    "use strict";
 
-        return Component.extend("bc.zdtimpfiles.Component", {
-            metadata: {
-                manifest: "json"
-            }
-        });
-    }
-);
+    return Component.extend("bc.zdtimpfiles.Component", {
+
+        ListReportExt: ListReportExt,
+
+        metadata: {
+            manifest: "json"
+        },
+
+        onAfterRendering: function () {
+            this.ListReportExt.getAuthorityData(this.oModels);
+        }
+    });
+});
