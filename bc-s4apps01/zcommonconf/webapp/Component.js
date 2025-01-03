952c934bb1de0c5370d5b86ec254402101bdfa92
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-01-03 08:26:07 +0000

    BC-001 BTP汎用管理 权限检查

--- bc-s4apps01/zcommonconf/webapp/Component.js
+++ bc-s4apps01/zcommonconf/webapp/Component.js
@@ -1,33 +1,39 @@
 sap.ui.define([
     "sap/fe/core/AppComponent",
-    "sap/m/MessageBox"
-], function (Component, MessageBox) {
+    "sap/m/MessageBox",
+    "bc/zcommonconf/ext/controller/ListReportExt"
+], function (Component, MessageBox, ListReportExt) {
     "use strict";
 
     return Component.extend("bc.zcommonconf.Component", {
+
+        ListReportExt: ListReportExt,
+
         metadata: {
             manifest: "json"
         },
 
         onAfterRendering: function () {
-            var sMessage = "为方便数据管理，请大家维护配置时遵循以下几点:"
-                + "\n\n"
-                + "1、请先维护共通配置流水表：https://docs.qq.com/sheet/DQWlGc1BSc1NLRnFi?tab=000001，点击确认自动跳转。"
-                + "\n\n"
-                + "2、命名规范：Z + 模块 + 三位流水。"
-                + "\n\n"
-                + "3、配置完成后，请仔细检查数据的正确性。"
-                + "\n\n"
-                + "谢谢大家配合。"
-                + "\n\n"
-                + "许鑫磊";
-            MessageBox.information(sMessage, {
-                actions: [MessageBox.Action.OK],
-                emphasizedAction: MessageBox.Action.OK,
-                onClose: function (sAction) {
-                    window.open("https://docs.qq.com/sheet/DQWlGc1BSc1NLRnFi?tab=000001");
-                }
-            });
+            // var sMessage = "为方便数据管理，请大家维护配置时遵循以下几点:"
+            //     + "\n\n"
+            //     + "1、请先维护共通配置流水表：https://docs.qq.com/sheet/DQWlGc1BSc1NLRnFi?tab=000001，点击确认自动跳转。"
+            //     + "\n\n"
+            //     + "2、命名规范：Z + 模块 + 三位流水。"
+            //     + "\n\n"
+            //     + "3、配置完成后，请仔细检查数据的正确性。"
+            //     + "\n\n"
+            //     + "谢谢大家配合。"
+            //     + "\n\n"
+            //     + "许鑫磊";
+            // MessageBox.information(sMessage, {
+            //     actions: [MessageBox.Action.OK],
+            //     emphasizedAction: MessageBox.Action.OK,
+            //     onClose: function (sAction) {
+            //         window.open("https://docs.qq.com/sheet/DQWlGc1BSc1NLRnFi?tab=000001");
+            //     }
+            // });
+
+            this.ListReportExt.getAuthorityData(this.oModels);
         }
     });
 });
---
+++ bc-s4apps01/zcommonconf/webapp/ext/controller/ListReportExt.js
@@ -0,0 +1,67 @@
+sap.ui.define([
+    "sap/m/BusyDialog",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], function (BusyDialog, MessageBox, MessageToast) {
+    'use strict';
+
+    return {
+
+        getAuthorityData: function (oModels) {
+            var oAuthorityModel = oModels.Authority;
+            var oLocalModel = oModels.local;
+            var oI18nModel = oModels.i18n;
+            var _UserInfo = sap.ushell.Container.getService("UserInfo");
+            var sUser = _UserInfo.getFullName() === undefined ? "" : _UserInfo.getFullName();
+            var sEmail = _UserInfo.getEmail() === undefined ? "" : _UserInfo.getEmail();
+            var oContextBinding = oAuthorityModel.bindContext("/User(Mail='" + sEmail + "',IsActiveEntity=true)", undefined, {
+                "$expand": "_AssignPlant,_AssignCompany,_AssignSalesOrg,_AssignPurchOrg,_AssignRole($expand=_UserRoleAccessBtn)"
+            });
+            oContextBinding.requestObject().then(function (context) {
+                var aAccessBtns = [],
+                    aAllAccessBtns = [];
+                if (context._AssignRole && context._AssignRole.length > 0) {
+                    context._AssignRole.forEach(role => {
+                        aAccessBtns.push(role._UserRoleAccessBtn);
+                    });
+                    aAllAccessBtns = aAccessBtns.flat();
+                }
+                if (!aAllAccessBtns.some(btn => btn.AccessId === "zcommonconf-View")) {
+                    if (!this.oErrorMessageDialog) {
+                        this.oErrorMessageDialog = new sap.m.Dialog({
+                            type: sap.m.DialogType.Message,
+                            state: "Error",
+                            content: new sap.m.Text({
+                                text: oI18nModel.getResourceBundle().getText("noAuthorityView", [sUser])
+                            })
+                        });
+                    }
+                    this.oErrorMessageDialog.open();
+                }
+                oLocalModel.setProperty("/authorityCheck", {
+                    button: {
+                        View: aAllAccessBtns.some(btn => btn.AccessId === "zcommonconf-View")
+                    },
+                    data: {
+                        PlantSet: context._AssignPlant,
+                        CompanySet: context._AssignCompany,
+                        SalesOrgSet: context._AssignSalesOrg,
+                        PurchOrgSet: context._AssignPurchOrg,
+                        RoleSet: context._AssignRole
+                    }
+                });
+            }.bind(this), function (oError) {
+                if (!this.oErrorMessageDialog) {
+                    this.oErrorMessageDialog = new sap.m.Dialog({
+                        type: sap.m.DialogType.Message,
+                        state: "Error",
+                        content: new sap.m.Text({
+                            text: oI18nModel.getResourceBundle().getText("getAuthorityFailed")
+                        })
+                    });
+                }
+                this.oErrorMessageDialog.open();
+            }.bind(this));
+        }
+    };
+});
