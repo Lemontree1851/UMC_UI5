e8d7c77bc2fe933161f5f1dbb968dd15a8d0bd01
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-01-09 01:53:23 +0000

    SD-031 与信管理表 机能ID修改

--- sd-s4apps01/creditmantablen/webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"sd/creditmantablen/test/unit/controller/Main.controller"
-], function () {
-	"use strict";
-});
---
+++ sd-s4apps01/zcreditmantablenew/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"sd/zcreditmantablenew/test/unit/controller/Main.controller"
+], function () {
+	"use strict";
+});
