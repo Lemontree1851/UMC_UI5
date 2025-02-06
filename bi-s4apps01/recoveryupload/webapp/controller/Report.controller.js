6b5cedd2c018013b3b3e5d2e2e232193458d329f
XINLEI XU <xinlei.xu@sh.shin-china.com>
2025-02-06 07:34:53 +0000

    BI-003 回収既存データ照会 追加删除逻辑

--- bi-s4apps01/recoveryupload/webapp/controller/Report.controller.js
+++ bi-s4apps01/recoveryupload/webapp/controller/Report.controller.js
@@ -2,8 +2,11 @@ sap.ui.define([
     "./Base",
     "../model/formatter",
     "sap/ui/model/Filter",
-    "sap/ui/model/FilterOperator"
-], function (Base, formatter, Filter, FilterOperator) {
+    "sap/ui/model/FilterOperator",
+    "sap/m/BusyDialog",
+    "sap/m/MessageBox",
+    "sap/m/MessageToast"
+], function (Base, formatter, Filter, FilterOperator, BusyDialog, MessageBox, MessageToast) {
     "use strict";
 
     return Base.extend("bi.recoveryupload.controller.Report", {
@@ -11,7 +14,7 @@ sap.ui.define([
         formatter: formatter,
 
         onInit() {
-
+            this._BusyDialog = new BusyDialog();
         },
 
         onBeforeRebindTable: function (oEvent) {
@@ -25,12 +28,12 @@ sap.ui.define([
             aNewFilters.push(new Filter("UploadType", FilterOperator.EQ, sUploadType));
 
             if (sYearMonth) {
-                aNewFilters.push(new Filter("YearMonth", FilterOperator.EQ, sYearMonth));
+                aNewFilters.push(new Filter("YearMonth", FilterOperator.EQ, sYearMonth.substring(0, 4) + "0" + sYearMonth.substring(4, 6)));
             }
             if (aNewFilters.length) {
                 oNewFilter = new Filter({
                     filters: aNewFilters,
-                    and: false
+                    and: true
                 });
                 aFilters.push(oNewFilter);
             }
@@ -56,9 +59,10 @@ sap.ui.define([
                 aItems.push(oRow);
             }
             var sTitleVariable = this.getModel("i18n").getResourceBundle().getText("Delete");
+            var sUploadType = this.getModel("local").getProperty("/filterUploadType");
             var oRequestData = {
                 UploadType: sUploadType,
-                JsonData: aExcelSet
+                JsonData: aItems
             };
             MessageBox.confirm(this.getModel("i18n").getResourceBundle().getText("ConfirmMessage", [sTitleVariable]), {
                 actions: [MessageBox.Action.OK, MessageBox.Action.CANCEL],
@@ -70,11 +74,13 @@ sap.ui.define([
                             "Zzkey": JSON.stringify(oRequestData),
                             "RecordUUID": ""
                         }, {}).then(function (oResponse) {
-                            var result = JSON.parse(oResponse.processLogic.Zzkey);
-
+                            if (oResponse.processLogic.Zzkey) {
+                                var result = JSON.parse(oResponse.processLogic.Zzkey);
+                            }
                             that._oTable.clearSelection();
                             that.getModel().resetChanges();
                             that.getModel().refresh();
+                            MessageToast.show(that.getModel("i18n").getResourceBundle().getText("ProcessingCompleted"));
                         }, function (oError) {
                             var sMsg;
                             if (oError.error.innererror.errordetails.length > 0) {
